3d8f46bc7bbb90e7ba643790ca4d6753f70307e6
jmjuanes <josemijuanes@gmail.com>
2017-10-05 18:10:49 +0200

    js/btn.js: init button component

---
+++ js/btn.js
@@ -0,0 +1,43 @@
+//Button class
+export default class SiimpleBtn extends React.Component
+{
+  //Constructor
+  constructor(props)
+  {
+    super(props);
+
+    //Set the state
+    this.state = { color: null, disabled: false, text: null };
+
+    //Parse the properties
+    if(typeof this.props.color === 'string'){ this.state.color = this.props.color; }
+
+    //Parse the disabled property
+    if(typeof this.props.disabled === 'boolean'){ this.state.disabled = this.props.disabled; }
+
+    //Parse the text property
+    if(typeof this.props.text === 'string'){ this.state.text = this.props.text; }
+  }
+
+  //Render the button
+  render(props, state)
+  {
+    //Save this
+    var self = this;
+
+    //Initialize the list of classes
+    var list = [ 'siimple-btn' ];
+
+    //Add the button color
+    if(typeof this.state.color === 'string'){ list.push('siimple-btn--' + this.state.color); }
+
+    //Add the button disabled option
+    if(this.state.disabled === true){ list.push('siimple-btn--disabled'); }
+
+    //Parse the text content
+    var text = (typeof this.state.text === 'string') ? this.state.text.trim() : '';
+
+    //Return the button element
+    return React.createElement('div', { className: list.join(' '), onClick: self.props.onClick }, text);
+  }
+}
