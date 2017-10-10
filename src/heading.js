d34f84941f5e9e513b880d70b7ca42b31457f6ba
jmjuanes <josemijuanes@gmail.com>
2017-10-10 17:40:51 +0200

    src/heading.js: init heading element

---
+++ src/heading.js
@@ -0,0 +1,23 @@
+//Heading class
+export default class SiimpleHeading extends React.Component
+{
+  //Constructor
+  constructor(props)
+  {
+    //Call super
+    super(props);
+
+    //Initialize the state object
+    this.state = { type: '1' };
+
+    //Parse the heading type value
+    if(typeof this.props.type === 'string'){ this.state.type = this.props.type; }
+  }
+
+  //Render the heading element
+  render()
+  {
+    //Return the heading element
+    return React.createElement('div', { className: 'siimple-h' + this.state.type }, this.props.children);
+  }
+}
