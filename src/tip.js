368c442282a1c44ccafbdf1ece8a2d64a2ef501f
jmjuanes <josemijuanes@gmail.com>
2017-10-13 16:45:58 +0200

    src/tip.js: init tip component

---
+++ src/tip.js
@@ -0,0 +1,45 @@
+//Tip class
+export default class SiimpleTip extends React.Component
+{
+  //Constructor
+  constructor(props)
+  {
+    //Call super
+    super(props);
+
+    //Set the state
+    this.state = { color: null };
+
+    //Update props
+    this.componentWillReceiveProps(props);
+  }
+
+  componentWillReceiveProps(props)
+  {
+    //Parse the color property
+    if(typeof props.color === 'string' && props.color !== this.state.color)
+    {
+      this.state.color = props.color;
+    }
+  }
+
+  //Render the tip element
+  render()
+  {
+    //Create element alias
+    var el = React.createElement;
+
+    //Initialize the tip class list
+    var class_list = [ 'siimple-tip' ];
+
+    //Add the tip color
+    if(typeof this.state.color === 'string')
+    {
+      //Append the color class
+      class_list.push('siimple-tip--' + this.state.color);
+    }
+
+    //Return the element
+    return el('div', { className: class_list.join(' ') }, this.props.children);
+  }
+}
