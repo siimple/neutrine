33e44b442e46579fdd1c6ba89a4084dcb8a1f71b
jmjuanes <josemijuanes@gmail.com>
2017-10-13 18:27:31 +0200

    src/heading.js: added method to parse properties when the element receive new props. Minor changes

--- src/heading.js
+++ src/heading.js
@@ -1,3 +1,5 @@
+import el from './element.js';
+
 //Heading class
 export default class SiimpleHeading extends React.Component
 {
@@ -10,21 +12,28 @@ export default class SiimpleHeading extends React.Component
     //Initialize the state object
     this.state = { type: '1' };
 
+    //Parse the initial properties
+    this.componentWillReceiveProps(props);
+  }
+
+  //Parse new props
+  componentWillReceiveProps(props)
+  {
     //Parse the heading type value
-    if(typeof this.props.type === 'string')
+    if(typeof props.type !== 'undefined' && props.type !== this.state.type)
     {
       //Save the heading type
-      this.state.type = this.props.type;
+      this.state.type = props.type;
     }
   }
 
   //Render the heading element
   render()
   {
-    //Create element alias
-    var el = React.createElement;
+    //Get the header class
+    var class_name = (typeof this.state.type === 'string') ? 'siimple-h' + this.state.type.trim() : null;
 
     //Return the heading element
-    return el('div', { className: 'siimple-h' + this.state.type }, this.props.children);
+    return el.div({ className: class_name }, this.props.children);
   }
 }
