8306f2944c3f66506c0b93e1ae5712f118628c8b
jmjuanes <josemijuanes@gmail.com>
2017-10-13 17:55:30 +0200

    src/alert.js: minor changes

--- src/alert.js
+++ src/alert.js
@@ -10,51 +10,52 @@ export default class SiimpleAlert extends React.Component
     //Initialize the state object
     this.state = { color: null, closeBtn: false };
 
+    //Parse the properties
+    this.componentWillReceiveProps(props);
+  }
+
+  //Parse new properties
+  componentWillReceiveProps(props)
+  {
     //Parse the color property
-    if(typeof this.props.color === 'string')
+    if(typeof props.color !== 'undefined' && props.color !== this.state.color)
     {
       //Save the alert color value
-      this.state.color = this.props.color.trim();
+      this.state.color = props.color;
     }
 
     //Parse the close button property
-    if(typeof this.props.closeBtn === 'boolean')
+    if(typeof props.closeBtn === 'boolean' && props.closeBtn !== this.state.closeBtn)
     {
       //Save the alert close button visibility value
-      this.state.closeBtn = this.props.closeBtn;
+      this.state.closeBtn = props.closeBtn;
     }
   }
 
   //Render the alert component
   render()
   {
-    //Save this
-    var self = this;
-
-    //Create element alias
-    var e = React.createElement;
-
-    //Initialize the class names
-    var className = [ 'siimple-alert' ];
+    //Initialize the list with the element classes
+    var class_list = [ 'siimple-alert' ];
 
     //Check the color
     if(typeof this.state.color === 'string')
     {
       //Add the alert color
-      className.push('siimple-alert--'  + this.state.color);
+      class_list.push('siimple-alert--'  + this.state.color.toLowerCase().trim());
     }
 
     //Initialize the child elements list
-    var children = [ e('span', { key: 0 }, this.props.children) ];
+    var children = [ React.createElement('span', { key: 0 }, this.props.children) ];
 
     //Check the close button
     if(this.state.closeBtn === true)
     {
       //Append the close button div
-      children.push(e('div', { className: 'siimple-close', key: 1 }));
+      children.push(React.createElement('div', { className: 'siimple-close', key: 1 }));
     }
 
     //Create the alert element
-    return e('div', { className: className.join(' ') }, children);
+    return React.createElement('div', { className: class_list.join(' ') }, children);
   }
 }
