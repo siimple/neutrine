73161496460c6bd80d6a529281a0d26af9d4edba
jmjuanes <josemijuanes@gmail.com>
2017-10-20 16:56:24 +0200

    src/hyperscript.js: init hyperscript parser

---
+++ src/hyperscript.js
@@ -0,0 +1,44 @@
+//Hyperscript method
+var hyperscript = function(tag, props, children)
+{
+  //Check the number of arguments
+  if(arguments.length === 2)
+  {
+    //Initialize an empty children object
+    children = [];
+  }
+  else if(arguments.length !== 3)
+  {
+    //Throw error and exit
+    throw new Error('Expected 3 arguments in hyperscript constructor');
+  }
+
+  //Check the children list
+  if(Array.isArray(children) === false){ children = [ children ]; }
+
+  //Create the element
+  var args = [ tag, props ].concat(children);
+
+  //Call the create element method
+  return React.createElement.apply(null, args);
+};
+
+//Create an element alias
+['div', 'span', 'button', 'b', 'i'].forEach(function(tag)
+{
+  //Register the element alias function
+  hyperscript[tag] = function()
+  {
+    //Convert the arguments to array
+    var args = Array.prototype.slice.call(arguments);
+
+    //Add the tag name
+    args.unshift(tag);
+
+    //Call the hyperscript method with this arguments
+    return hyperscript.apply(null, args);
+  }
+});
+
+//Exports the hyperscript object
+export default hyperscript;
