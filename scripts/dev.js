7e938814f026406fbf6f7f81120eb5d53fc8f2e8
jmjuanes <josemijuanes@gmail.com>
2017-11-09 15:56:27 +0100

    scripts/dev.js: fixed develop server

--- scripts/dev.js
+++ scripts/dev.js
@@ -3,26 +3,20 @@ var express = require('express');
 var path = require('path');
 var rollupMiddleware = require('rollup-middleware');
 
-//Rollup plugins
-var nodeResolve = require('rollup-plugin-node-resolve');
-var bowerResolve = require('rollup-plugin-bower-resolve');
-
 //Initialize the express application
 var app = express();
 
 //Initialize the rollup options
-var options = { rollup: {}, generate: {} };
-options.rollup.plugins = [ bowerResolve(), nodeResolve() ];
-options.generate.format = 'es';
+var rollup_options = { rollup: {}, generate: { format: 'es' } };
 
 //Use the rollup middleware
-app.use(rollupMiddleware(options));
+app.use(rollupMiddleware(rollup_options));
 
 //Serve static files
-app.use(express.static(path.join(process.cwd(), './examples/')));
+app.use('/examples/', express.static('./examples/'));
 
 //Map the bower components folder
-app.use('/bower_components', express.static(path.join(process.cwd(), './bower_components/')));
+app.use('/bower_components', express.static('./bower_components/'));
 
 //Run the server
 app.listen(5000, function()
