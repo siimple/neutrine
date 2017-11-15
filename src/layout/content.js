784a9145ce80a53dfdb343d913d9feb13ef00957
jmjuanes <josemijuanes@gmail.com>
2017-11-15 18:09:31 +0100

    src/layout/content.js: removed content sizes. Import global layout sizes

--- src/layout/content.js
+++ src/layout/content.js
@@ -1,8 +1,7 @@
 import h from '../hyperscript.js';
 import SiimpleComponent from '../index.js';
 
-//Content sizes
-var content_sizes = ['small', 'large', 'fluid'];
+import sizes from './sizes.js';
 
 //Content layout component
 export default class SiimpleContent extends SiimpleComponent
@@ -14,7 +13,7 @@ export default class SiimpleContent extends SiimpleComponent
     var class_list = [ 'siimple-content' ];
 
     //Check the content size
-    if(typeof this.props.size === 'string' && content_sizes.indexOf(this.props.size.toLowerCase()) !== -1)
+    if(typeof this.props.size === 'string' && sizes.indexOf(this.props.size.toLowerCase()) !== -1)
     {
       //Add the size class
       class_list.push('siimple-content--' + this.props.size.toLowerCase());
