22252a4f68d233dc13c49f47194dfee536cc7f6a
jmjuanes <josemijuanes@gmail.com>
2017-11-21 12:38:00 +0100

    index.js: import and export all components

---
+++ index.js
@@ -0,0 +1,46 @@
+import SiimpleAlert from './src/elements/alert.js';
+import SiimpleBtn from './src/elements/btn.js';
+import SiimpleClose from './src/elements/close.js';
+import SiimpleSpinner from './src/elements/spinner.js';
+import SiimpleTip from './src/elements/tip.js';
+
+import { SiimpleForm, SiimpleFormField } from './src/form/form.js';
+import SiimpleInput from './src/form/input.js';
+import SiimpleLabel from './src/form/label.js';
+import SiimpleSelect from './src/form/select.js';
+import SiimpleSwitch from './src/form/switch.js';
+
+import { SiimpleGrid, SiimpleGridRow, SiimpleGridCol } from './src/grid/grid.js';
+
+import SiimpleBox from './src/layout/box.js';
+import SiimpleContent from './src/layout/content.js';
+import SiimpleFooter from './src/layout/footer.js';
+import SiimpleJumbotron from './src/layout/jumbotron.js';
+import { SiimpleNavbar, SiimpleNavbarTitle, SiimpleNavbarLink } from './src/layout/navbar.js';
+
+import SiimpleHeading from './src/typography/heading.js';
+
+export default {
+  SiimpleAlert,
+  SiimpleBtn,
+  SiimpleClose,
+  SiimpleSpinner,
+  SiimpleTip,
+  SiimpleForm,
+  SiimpleFormField,
+  SiimpleInput,
+  SiimpleLabel,
+  SiimpleSelect,
+  SiimpleSwitch,
+  SiimpleGrid,
+  SiimpleGridRow,
+  SiimpleGridCol,
+  SiimpleBox,
+  SiimpleContent,
+  SiimpleFooter,
+  SiimpleJumbotron,
+  SiimpleNavbar,
+  SiimpleNavbarTitle,
+  SiimpleNavbarLink,
+  SiimpleHeading
+}
