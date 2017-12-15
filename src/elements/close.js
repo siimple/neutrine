4038cac09da9fa54b32f932b0c84edcbc7ba4d49
jmjuanes <josemijuanes@gmail.com>
2017-12-16 00:01:40 +0100

    src/elements/close.js: minor changes. Removed unused click handler

--- src/elements/close.js
+++ src/elements/close.js
@@ -4,32 +4,14 @@ import SiimpleComponent from '../index.js';
 //Close button class
 export default class SiimpleClose extends SiimpleComponent
 {
-  //Constructor
-  constructor(props)
-  {
-    //Call the super constructor
-    super(props);
-
-    //Bind the handle click method
-    this.handleClick = this.handleClick.bind(this);
-  }
-
-  //Handle click method
-  handleClick(e)
-  {
-    //Check if the onclick prop is defined
-    if(typeof this.props.onClick === 'function')
-    {
-      //Call the onclick method
-      return this.props.onClick.call(null, e);
-    }
-  }
-
   //Render the close button component
   render()
   {
+    //Save this
+    var self = this;
+
     //Render the close button
-    return h.div({ className: 'siimple-close', onClick: this.handleClick }, null);
+    return h('div', { className: 'siimple-close', onClick: self.props.onClick, style: self.props.style });
   }
 }
 
