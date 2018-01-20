adae710b7393856b77708772447fe20a24adfc87
jmjuanes <josemijuanes@gmail.com>
2018-01-20 21:47:12 +0100

    Export menu components

--- index.js
+++ index.js
@@ -11,47 +11,51 @@ import SiimpleLabel from './src/form/label.js';
 import SiimpleSelect from './src/form/select.js';
 import SiimpleSwitch from './src/form/switch.js';
 
-import { SiimpleGrid, SiimpleGridRow, SiimpleGridCol } from './src/grid/grid.js';
+import {SiimpleGrid, SiimpleGridRow, SiimpleGridCol} from './src/grid/grid.js';
 
 import SiimpleBox from './src/layout/box.js';
-import { SiimpleBreadcrumb, SiimpleBreadcrumbCrumb } from './src/layout/breadcrumb.js';
+import {SiimpleBreadcrumb, SiimpleBreadcrumbCrumb} from './src/layout/breadcrumb.js';
 import SiimpleContent from './src/layout/content.js';
 import SiimpleFooter from './src/layout/footer.js';
 import SiimpleJumbotron from './src/layout/jumbotron.js';
-import { SiimpleNavbar, SiimpleNavbarTitle, SiimpleNavbarLink } from './src/layout/navbar.js';
+import {SiimpleMenu, SiimpleMenuGroup, SiimpleMenuItem} from './src/layout/menu.js';
+import {SiimpleNavbar, SiimpleNavbarTitle, SiimpleNavbarLink} from './src/layout/navbar.js';
 import SiimpleTabs from './src/layout/tabs.js';
 
 import SiimpleHeading from './src/typography/heading.js';
 import SiimpleParagraph from './src/typography/paragraph.js';
 
 export {
-  SiimpleAlert,
-  SiimpleBtn,
-  SiimpleClose,
-  SiimpleSpinner,
-  SiimpleTip,
-  SiimpleForm,
-  SiimpleFormField,
-  SiimpleInput,
-  SiimpleLabel,
-  SiimpleSelect,
-  SiimpleSwitch,
+    SiimpleAlert,
+    SiimpleBtn,
+    SiimpleClose,
+    SiimpleSpinner,
+    SiimpleTip,
+    SiimpleForm,
+    SiimpleFormField,
+    SiimpleInput,
+    SiimpleLabel,
+    SiimpleSelect,
+    SiimpleSwitch,
 
-  SiimpleGrid,
-  SiimpleGridRow,
-  SiimpleGridCol,
+    SiimpleGrid,
+    SiimpleGridRow,
+    SiimpleGridCol,
 
-  SiimpleBox,
-  SiimpleBreadcrumb,
-  SiimpleBreadcrumbCrumb,
-  SiimpleContent,
-  SiimpleFooter,
-  SiimpleJumbotron,
-  SiimpleNavbar,
-  SiimpleNavbarTitle,
-  SiimpleNavbarLink,
-  SiimpleTabs,
+    SiimpleBox,
+    SiimpleBreadcrumb,
+    SiimpleBreadcrumbCrumb,
+    SiimpleContent,
+    SiimpleFooter,
+    SiimpleJumbotron,
+    SiimpleMenu,
+    SiimpleMenuGroup,
+    SiimpleMenuItem,
+    SiimpleNavbar,
+    SiimpleNavbarTitle,
+    SiimpleNavbarLink,
+    SiimpleTabs,
 
-  SiimpleHeading,
-  SiimpleParagraph
+    SiimpleHeading,
+    SiimpleParagraph
 }
