7009938a7914ca77732cee69b1fa2fe2535ae4d1
jmjuanes <josemijuanes@gmail.com>
2018-04-04 16:38:17 +0200

    Renamed spinner component. Minor code reformat

--- src/elements/spinner.js
+++ src/elements/spinner.js
@@ -1,33 +1,27 @@
-import h from '../hyperscript.js';
-import SiimpleComponent from '../index.js';
+import React from "react";
+import {hyperscript as h} from "neutrine-utils";
 
 //Spinner class
-export default class SiimpleSpinner extends SiimpleComponent
-{
-  //Render the spinner element
-  render()
-  {
-    //Initialize the spinner class
-    var class_list = [ 'siimple-spinner' ];
-
-    //Check the color attribute
-    if(typeof this.props.color === 'string')
-    {
-      //Add the color class
-      class_list.push('siimple-spinner--' + this.props.color);
+export default class Spinner extends React.Component {
+    render() {
+        let classList = ["siimple-spinner"];
+        //Check the color attribute
+        if(typeof this.props.color === "string") {
+            classList.push("siimple-spinner--" + this.props.color);
+        }
+        //Check the size attribute
+        if (typeof this.props.size === "string") {
+            classList.push("siimple-spinner--" + this.props.size);
+        }
+        //Return the spinner element
+        return h("div", {className: classList, style: this.props.style}, null);
     }
-
-    //Check the size attribute
-    if(typeof this.props.size === 'string')
-    {
-      //Add the size class
-      class_list.push('siimple-spinner--' + this.props.size);
-    }
-
-    //Return the element
-    return h('div', { className: class_list, style: this.props.style }, null);
-  }
 }
 
 //Spinner default props
-SiimpleSpinner.defaultProps = { color: 'blue', size: null, style: null };
+Spinner.defaultProps = { 
+    color: "blue", 
+    size: null, 
+    style: null 
+};
+
