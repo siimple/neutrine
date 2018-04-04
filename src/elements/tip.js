cd1fb2cbff4ad1778112a9781e302548ae2dc155
jmjuanes <josemijuanes@gmail.com>
2018-04-04 16:47:44 +0200

    Renamed tip element. Big code reformat

--- src/elements/tip.js
+++ src/elements/tip.js
@@ -1,43 +1,27 @@
-import h from '../hyperscript.js';
-import SiimpleComponent from '../index.js';
+import React from "react";
+import {hyperscript as h} from "neutrine-utils";
 
 //Tip class
-export default class SiimpleTip extends SiimpleComponent
-{
-  //Render the tip element
-  render()
-  {
-    //Initialize the tip class list
-    var class_list = [ 'siimple-tip' ];
-
-    //Initialize the available icons
-    var icons = [ 'exclamation', 'question', 'hearth' ];
-
-    //Add the tip color
-    if(typeof this.props.color === 'string')
-    {
-      //Append the color class
-      class_list.push('siimple-tip--' + this.props.color.toLowerCase().trim());
+export default class Tip extends React.Component {
+    render() {
+        let classList = ["siimple-tip"];
+        //Add the tip color
+        if (typeof this.props.color === "string") {
+            classList.push("siimple-tip--" + this.props.color.toLowerCase().trim());
+        }
+        //Add the tip icon
+        if (typeof this.props.icon === "string") {
+            classList.push("siimple-tip--" + this.props.icon.toLowerCase().trim());
+        }
+        //Return the tip element
+        return h("div", {className: classList, style: this.props.style}, this.props.children);
     }
-
-    //Add the tip icon
-    if(typeof this.props.icon === 'string')
-    {
-      //Parse the icon value
-      var icon_value = this.props.icon.toLowerCase().trim();
-
-      //Check if this icon exists
-      if(icons.indexOf(icon_value) !== -1)
-      {
-        //Append the icon class
-        class_list.push('siimple-tip--' + icon_value);
-      }
-    }
-
-    //Return the element
-    return h('div', { className: class_list, style: this.props.style }, this.props.children);
-  }
 }
 
 //Initialize the tip element default props
-SiimpleTip.defaultProps = { color: 'blue', icon: null, style: null };
+Tip.defaultProps = { 
+    color: "blue", 
+    icon: null, 
+    style: null
+};
+
