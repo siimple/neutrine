2608c4d4c79fb5924ce802669b8436101c3a867b
jmjuanes <josemijuanes@gmail.com>
2018-04-04 16:23:46 +0200

    Minor changes

--- src/elements/alert.js
+++ src/elements/alert.js
@@ -8,19 +8,16 @@ import "siimple/css/elements/_alert.scss";
 export class Alert extends React.Component {
     render() {
         let self = this;
-        //Initialize the list with the element classes
         let classList = [ "siimple-alert" ];
+        let children = [h("span", {}, this.props.children)];
         //Check the alert color property
         if (typeof this.props.color === "string") {
             class_list.push("siimple-alert--"  + this.props.color.toLowerCase().trim());
         }
-        //Initialize the child elements list
-        let children = [h("span", {}, this.props.children)];
         //Check if the close button is enabled
         if (this.props.showClose === true) {
             children.push(h(Close, {onClick: self.props.onCloseClick}));
         }
-        //Return the alert
         return h("div", {className: classList, style: this.props.style}, children);
     }
 }
