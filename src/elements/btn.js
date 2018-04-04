5a8467c89333aa7acfad9691f591d2db9886ea3a
jmjuanes <josemijuanes@gmail.com>
2018-04-04 16:35:08 +0200

    Renamed button component. Big code reformat

--- src/elements/btn.js
+++ src/elements/btn.js
@@ -1,53 +1,43 @@
-import SiimpleComponent from '../index.js';
-import h from '../hyperscript.js';
+import React from "react";
+import {hyperscript as h} from "neutrine-utils";
 
-//Button class
-export default class SiimpleBtn extends SiimpleComponent
-{
-  //Render the button
-  render()
-  {
-    //Save this
-    var self = this;
-
-    //Initialize the button props
-    var btn_props = { onClick: self.props.onClick, style: {} };
-
-    //Initialize the list of classes
-    btn_props.className = [ 'siimple-btn' ];
-
-    //Add the button color
-    if(typeof this.props.color === 'string')
-    {
-      //Add button color class
-      btn_props.className.push('siimple-btn--' + this.props.color.toLowerCase().trim());
+//Button component
+export default class Btn extends React.Component {
+    render() {
+        let self = this;
+        let btnProps = {
+            className: ["siimple-btn"],
+            onClick: self.props.onClick, 
+            style: {}
+        };
+        //Add the button color
+        if (typeof this.props.color === "string") {
+            btnProps.className.push("siimple-btn--" + this.props.color.toLowerCase().trim());
+        }
+        //Add the button disabled option
+        if (this.props.disabled === true) {
+            btnProps.className.push("siimple-btn--disabled");
+        }
+        //Check the fluid property
+        if (this.props.fluid === true) {
+            //btn_props.style = { width: '100%', paddingLeft: '0px', paddingRight: '0px' };
+            btnProps.className.push("siimple-btn--fluid");
+        }
+        //Check the custom styles
+        if (typeof this.props.style === "object" && this.props.style !== null) {
+            btnProps.style = Object.assign(btnProps.style, this.props.style);
+        }
+        //Return the button element
+        return h("div", btnProps, this.props.children);
     }
-
-    //Add the button disabled option
-    if(this.props.disabled === true)
-    {
-      //Add button disabled class
-      btn_props.className.push('siimple-btn--disabled');
-    }
-
-    //Check the fluid property
-    if(this.props.fluid === true)
-    {
-      //Change the button style
-      btn_props.style = { width: '100%', paddingLeft: '0px', paddingRight: '0px' };
-    }
-
-    //Check the custom styles
-    if(typeof this.props.style === 'object' && this.props.style !== null)
-    {
-      //Assign the new styles
-      btn_props.style = Object.assign(btn_props.style, this.props.style);
-    }
-
-    //Return the button element
-    return h('div', btn_props, this.props.children);
-  }
 }
 
 //Default properties values
-SiimpleBtn.defaultProps = { color: 'blue', disabled: false, onClick: null, style: null, fluid: false };
+Btn.defaultProps = { 
+    color: "blue", 
+    disabled: false, 
+    onClick: null, 
+    style: null, 
+    fluid: false 
+};
+
