02d83585fb6328901f868dc68616defc90ad8525
jmjuanes <josemijuanes@gmail.com>
2018-04-05 11:12:37 +0200

    Added style as props in list, list-item and list-title components

--- src/elements/list.js
+++ src/elements/list.js
@@ -11,26 +11,33 @@ export class List extends React.Component {
         if (this.props.hover === true) {
             classList.push("siimple-list--hover");
         }
-        return h("div", {className: classList}, this.props.children);
+        return h("div", {className: classList, style: this.props.style}, this.props.children);
     }
 }
 
 //List default props 
 List.defaultProps = {
-    hover: false
+    hover: false,
+    style: null
 };
 
 //List item component 
 export class ListItem extends React.Component {
     render() {
-        return h("div", {className: "siimple-list-item"}, this.props.children);
+        return h("div", {className: "siimple-list-item", style: this.props.style}, this.props.children);
     }
 }
+
+//List item default props 
+ListItem.defaultProps = {style: null};
 
 //List title component 
 export class ListTitle extends React.Component {
     render() {
-        return h("div", {className: "siimple-list-title"}, this.props.children);
+        return h("div", {className: "siimple-list-title", style: this.props.style}, this.props.children);
     }
 }
+
+//List title default props 
+ListTitle.defaultProps = {style: null};
 
