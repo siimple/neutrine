c67cf4bb869d58a5c9e862813c86231730b21ba3
jmjuanes <josemijuanes@gmail.com>
2018-04-05 17:41:20 +0200

    Removed options attribute --> now options are passed as children

--- src/form/select.js
+++ src/form/select.js
@@ -8,25 +8,20 @@ import "siimple/scss/form/_select.scss";
 export default class Select extends React.Component {
     render() {
         //Clone the select props
-        let props = omit(this.props, ["fluid", "options", "children"])
+        let props = omit(this.props, ["fluid", "children"])
         //Initialize the select class
         props.className = ["siimple-select"];
         //Check the fluid property
         if (this.props.fluid === true) {
             props.className.push("siimple-select--fluid");
         }
-        //Parse all the options of the select
-        let options = this.props.options.map(function (opt) {
-            return h("option", {value: opt.value}, opt.name);
-        });
         //Return the select element
-        return h("select", props, options);
+        return h("select", props, this.props.children);
     }
 }
 
 //Select element default props
 Select.defaultProps = {
-    options: [], 
     fluid: false, 
     style: null 
 };
