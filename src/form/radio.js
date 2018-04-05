35b3fca5911a4d27195792640809de230c6497d9
jmjuanes <josemijuanes@gmail.com>
2018-04-05 13:44:31 +0200

    Init radio component

---
+++ src/form/radio.js
@@ -0,0 +1,74 @@
+import React from "react";
+import {hyperscript as h} from "neutrine-utils";
+
+import "siimple/scss/form/_radio.scss";
+
+//Generate a random ID
+let randomID = function () {
+    return Math.random().toString(36).slice(2, 9) + Date.now().toString(36); 
+};
+
+//Radio component 
+export default class Radio extends React.Component {
+     constructor(props) {
+        super(props);
+        this.state = {
+            id: randomID() 
+        };
+        //Initialize the referenced elements object
+        this.ref = {};
+        //Bind handlers
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    //Handle the radio change
+    handleChange(e) {
+        if (typeof this.props.onChange === "function") {
+            this.props.onChange.call(null, e.nativeEvent, this.ref.input.checked); 
+        }
+    }
+
+    //Get or set the radio state
+    checked(isChecked) {
+        //Check the is checked value
+        if (typeof isChecked === "boolean") {
+            //Set the checked value
+            this.ref.input.checked = isChecked;
+        } else {
+            //Return if radio is checked
+            return this.ref.input.checked;
+        }
+    }
+
+    //Render the radio element
+    render() {
+        let self = this;
+        //Input default props
+        let inputProps = {
+            id: this.state.id,
+            type: "radio",
+            defaultChecked: this.props.checked,
+            name: this.props.name,
+            ref: function (i) {
+                self.ref.input = i;
+            },
+            onChange: self.handleChange
+        };
+        //Switch children content
+        let children = [
+            h("input", inputProps, null),
+            h("label", {htmlFor: this.state.id}, null)
+        ];
+        //Return the radio element
+        return h("div", {className: "siimple-radio", style: this.props.style}, children);
+    } 
+}
+
+//Radio component default props
+Radio.defaultProps = {
+    name: null, 
+    checked: true, 
+    onChange: null,
+    style: null
+};
+
