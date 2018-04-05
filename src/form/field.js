f81fcb1556bac77a4b0bbf2d5159b04c642d94f2
jmjuanes <josemijuanes@gmail.com>
2018-04-05 11:59:14 +0200

    Renamed field component. Minor code reformat

--- src/form/field.js
+++ src/form/field.js
@@ -1,40 +1,31 @@
-import h from '../hyperscript.js';
-import SiimpleComponent from '../index.js';
+import React from "react";
+import {hyperscript as h} from "neutrine-utils";
+
+import "siimple/scss/form/_field.scss";
 
 //Form field component
-export default class SiimpleFormField extends SiimpleComponent
-{
-  //Render the form field element
-  render()
-  {
-    //Initialize the form field children
-    var children = [];
-
-    //Check the label prop
-    if(typeof this.props.label === 'string')
-    {
-      //Append the label text
-      children.push(h.div({ className: 'siimple-form-field-label' }, this.props.label));
+export default class Field extends React.Component {
+    render() {
+        let children = [];
+        //Check the label prop
+        if (typeof this.props.label === "string") {
+            children.push(h("div", {className: "siimple-form-field-label"}, this.props.label));
+        }
+        //Append the children
+        if (typeof this.props.children !== "undefined") {
+            children = children.concat(this.props.children);
+        }
+        //Check the helper prop
+        if (typeof this.props.helper === "string") {
+            children.push(h("div", {className: "siimple-form-field-helper"}, this.props.helper));
+        }
+        return h("div", {className: "siimple-form-field", style: this.props.style}, children);
     }
-
-    //Append the children
-    if(typeof this.props.children !== 'undefined')
-    {
-      //Append the children array
-      children = children.concat(this.props.children);
-    }
-
-    //Check the helper prop
-    if(typeof this.props.helper === 'string')
-    {
-      //Append the helper text
-      children.push(h.div({ className: 'siimple-form-field-helper' }, this.props.helper));
-    }
-
-    //Render the field element
-    return h.div({ className: 'siimple-form-field' }, children);
-  }
 }
 
 //Form field default props
-SiimpleFormField.defaultProps = { label: null, helper: null };
+Field.defaultProps = { 
+    label: null, 
+    helper: null,
+    style: null
+};
