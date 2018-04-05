5ecbe9eb8519e0b63409e8a9a1aad2a22aedd874
jmjuanes <josemijuanes@gmail.com>
2018-04-05 12:44:55 +0200

    Renamed select component. Reformat code

--- src/form/select.js
+++ src/form/select.js
@@ -1,91 +1,78 @@
-import h from '../hyperscript.js';
-import SiimpleComponent from '../index.js';
+import React from "react";
+import {hyperscript as h} from "neutrine-utils";
 
-//Select element
-export default class SiimpleSelect extends SiimpleComponent
-{
-  //Constructor
-  constructor(props)
-  {
-    //Call super method
-    super(props);
+import "siimple/scss/form/_select.scss";
 
-    //Initialize the referenced elements object
-    this.ref = { select: null };
-
-    //Bind methods
-    this.handleChange = this.handleChange.bind(this);
-  }
-
-  //Handle select change
-  handleChange(e)
-  {
-    //Call the change listener
-    this.props.onChange.call(null, e.nativeEvent, this.value());
-  }
-
-  //Get or set the actual value
-  value(val)
-  {
-    //Check the provided value
-    if(typeof val !== 'undefined')
-    {
-      //Set the current value
-      this.ref.select.value = val;
-    }
-    else
-    {
-      //Return the input current value
-      return this.ref.select.value;
-    }
-  }
-
-  //Render the element
-  render()
-  {
-    //Save this
-    var self = this;
-
-    //Initialize the select props
-    var select_props = { className: [ 'siimple-select' ], name: this.props.name, style: this.props.style };
-
-    //Add the reference function
-    select_props.ref = function(s){ self.ref.select = s; };
-
-    //Check the disabled property
-    if(this.props.disabled === true)
-    {
-      //Add the disabled class
-      select_props.className.push('siimple-select--disabled');
+//Select component
+export default class Select extends React.Component {
+    constructor(props) {
+        super(props);
+        //Initialize the referenced elements object
+        this.ref = { select: null };
+        //Bind methods
+        this.handleChange = this.handleChange.bind(this);
     }
 
-    //Check the fluid property
-    if(this.props.fluid === true)
-    {
-      //Add the fluid class
-      select_props.className.push('siimple-select--fluid');
+    //Handle select change
+    handleChange(e) {
+        this.props.onChange.call(null, e.nativeEvent, this.value());
     }
 
-    //Check the default value
-    if(typeof this.props.value === 'string'){ select_props.defaultValue = this.props.value; }
+    //Get or set the actual value
+    value(val) {
+        if(typeof val !== "undefined") {
+            //Set the current value
+            this.ref.select.value = val;
+        } else {
+            //Return the input current value
+            return this.ref.select.value;
+        }
+    }
 
-    //Register the change listener
-    if(typeof this.props.onChange === 'function'){ select_props.onChange = self.handleChange; }
-
-    //Initialize the select children
-    var select_children = [];
-
-    //Parse all the provided options
-    this.props.options.forEach(function(opt)
-    {
-      //Register the option
-      select_children.push(h('option', { value: opt.value }, opt.name));
-    });
-
-    //Return the select element
-    return h('select', select_props, select_children);
-  }
+    //Render the element
+    render() {
+        let self = this;
+        let props = {
+            className: ["siimple-select"], 
+            name: this.props.name, 
+            style: this.props.style,
+            ref: function (s) {
+                self.ref.select = s;
+            }
+        };
+        //Check the disabled property
+        if (this.props.disabled === true) {
+            props.className.push("siimple-select--disabled");
+        }
+        //Check the fluid property
+        if (this.props.fluid === true) {
+            props.className.push("siimple-select--fluid");
+        }
+        //Check the default value
+        if (typeof this.props.value === "string") { 
+            props.defaultValue = this.props.value; 
+        }
+        //Register the change listener
+        if (typeof this.props.onChange === "function") { 
+            props.onChange = self.handleChange; 
+        }
+        //Parse all the options of the select
+        let options = this.props.options.map(function (opt) {
+            return h("option", {value: opt.value}, opt.name);
+        });
+        //Return the select element
+        return h("select", props, options);
+    }
 }
 
 //Select element default props
-SiimpleSelect.defaultProps = { name: null, options: [], value: null, onChange: null, disabled: false, fluid: false, style: null };
+Select.defaultProps = {
+    name: null, 
+    options: [], 
+    value: null, 
+    onChange: null, 
+    disabled: false, 
+    fluid: false, 
+    style: null 
+};
+
