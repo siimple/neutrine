00d1abfd2bdfa5a762aba908f1e67739fc1e69f2
jmjuanes <josemijuanes@gmail.com>
2018-04-05 16:43:37 +0200

    Big rewrite

--- src/form/select.js
+++ src/form/select.js
@@ -1,60 +1,19 @@
 import React from "react";
 import {hyperscript as h} from "neutrine-utils";
+import {omit} from "kofi";
 
 import "siimple/scss/form/_select.scss";
 
 //Select component
 export default class Select extends React.Component {
-    constructor(props) {
-        super(props);
-        //Initialize the referenced elements object
-        this.ref = { select: null };
-        //Bind methods
-        this.handleChange = this.handleChange.bind(this);
-    }
-
-    //Handle select change
-    handleChange(e) {
-        this.props.onChange.call(null, e.nativeEvent, this.value());
-    }
-
-    //Get or set the actual value
-    value(val) {
-        if(typeof val !== "undefined") {
-            //Set the current value
-            this.ref.select.value = val;
-        } else {
-            //Return the input current value
-            return this.ref.select.value;
-        }
-    }
-
-    //Render the element
     render() {
-        let self = this;
-        let props = {
-            className: ["siimple-select"], 
-            name: this.props.name, 
-            style: this.props.style,
-            ref: function (s) {
-                self.ref.select = s;
-            }
-        };
-        //Check the disabled property
-        if (this.props.disabled === true) {
-            props.className.push("siimple-select--disabled");
-        }
+        //Clone the select props
+        let props = omit(this.props, ["fluid", "options", "children"])
+        //Initialize the select class
+        props.className = ["siimple-select"];
         //Check the fluid property
         if (this.props.fluid === true) {
             props.className.push("siimple-select--fluid");
-        }
-        //Check the default value
-        if (typeof this.props.value === "string") { 
-            props.defaultValue = this.props.value; 
-        }
-        //Register the change listener
-        if (typeof this.props.onChange === "function") { 
-            props.onChange = self.handleChange; 
         }
         //Parse all the options of the select
         let options = this.props.options.map(function (opt) {
@@ -67,11 +26,7 @@ export default class Select extends React.Component {
 
 //Select element default props
 Select.defaultProps = {
-    name: null, 
     options: [], 
-    value: null, 
-    onChange: null, 
-    disabled: false, 
     fluid: false, 
     style: null 
 };
