42722743131865b8c824142b3d247dc09839c17d
jmjuanes <josemijuanes@gmail.com>
2018-04-05 12:37:04 +0200

    Fixed indentation

--- src/form/label.js
+++ src/form/label.js
@@ -5,9 +5,9 @@ import "siimple/scss/form/_label.scss";
 
 //Label component
 export default class Label extends React.Component {
-  render() {
-    return h("label", {className: "siimple-label", style: this.props.style}, this.props.children);
-  }
+    render() {
+        return h("label", {className: "siimple-label", style: this.props.style}, this.props.children);
+    }
 }
 
 //Label default props
