efd1edcb7a4689cce3b4a7dcaa9e8a03d85c7688
jmjuanes <josemijuanes@gmail.com>
2018-04-06 13:05:53 +0200

    Big rewrite. Added jumbotron title, subtitle and detail components

--- src/layout/jumbotron.js
+++ src/layout/jumbotron.js
@@ -1,47 +1,81 @@
 import React from "react";
 import {hyperscript as h} from "neutrine-utils";
+import {concat, omit} from "kofi";
 
 import "siimple/scss/layout/_jumbotron.scss";
 
 //Jumbotron base component
-export default class Jumbotron extends React.Component {
-  render() {
-    //Initialize the jumbotron class list
-    let classList = ["siimple-jumbotron"];
-    //Check the jumbotron color
-    if (typeof this.props.color === "string") {
-      classList.push("siimple-jumbotron--" + this.props.color.toLowerCase());
+export class Jumbotron extends React.Component {
+    render() {
+        //Clone the jumbotron props 
+        let props = omit(this.props, ["children", "className", "color", "size"]);
+        //Initialize the jumbotron class names list
+        props.className = concat(["siimple-jumbotron"], this.props.className);
+        //Check the jumbotron color
+        if (typeof this.props.color === "string") {
+            props.className.push("siimple-jumbotron--" + this.props.color.toLowerCase());
+        }
+        //Check the jumbotron size property
+        if (typeof this.props.size === "string") {
+            props.className.push("siimple-jumbotron--" + this.props.size.toLowerCase());
+        }
+        //Return the parent div
+        return h("div", props, this.props.children);
     }
-    //Check the jumbotron size property
-    if (typeof this.props.size === "string") {
-      classList.push("siimple-jumbotron--" + this.props.size.toLowerCase());
-    }
-    //Initialize the jumbotron children array
-    let children = [];
-    //Check the title property
-    if (typeof this.props.title === "string") {
-      children.push(h.div({ className: "siimple-jumbotron-title" }, this.props.title));
-    }
-    //Check the subtitle property
-    if (typeof this.props.subtitle === "string") {
-      children.push(h.div({ className: "siimple-jumbotron-subtitle" }, this.props.subtitle));
-    }
-    //Check the detail property
-    if (typeof this.props.detail === "string") {
-      children.push(h.div({ className: "siimple-jumbotron-detail" }, this.props.detail));
-    }
-    //Return the parent div
-    return h("div", {className: classList, style: this.props.style}, children, this.props.children);
-  }
 }
 
 //Jumbotron default props
 Jumbotron.defaultProps = {
     color: null, 
     size: null, 
-    style: null, 
-    title: null, 
-    subtitle: null, 
-    detail: null 
+    style: null,
+    className: []
+};
+
+//Jumbotron title
+export class JumbotronTitle extends React.Component {
+    render() {
+        //Clone the jumbotron props
+        let props = omit(this.props, ["children", "className"]);
+        props.className = concat(["siimple-jumbotron-title"], this.props.className);
+        //Return the jumbotron title element
+        return h("div", props, this.props.children);
+    }
+}
+
+//Jumbotron title default props 
+JumbotronTitle.defaultProps = {
+    style: null,
+    className: []
+};
+
+//Jumbotron subtitle 
+export class JumbotronSubtitle extends React.Component {
+    render() {
+        let props = omit(this.props, ["children", "className"]);
+        props.className = concat(["siimple-jumbotron-subtitle"], this.props.className);
+        return h("div", props, this.props.children);
+    }
+}
+
+//Jumbotron subtitle default props 
+JumbotronSubtitle.defaultProps = {
+    style: null,
+    className: []
+};
+
+//Jumbotron detail component 
+export class JumbotronDetail extends React.Component {
+    render() {
+        let props = omit(this.props, ["children", "className"]);
+        props.className = concat(["siimple-jumbotron-detail"], this.props.className);
+        return h("div", props, this.props.children);
+    }
+}
+
+//Jumbotron detail default props 
+JumbotronDetail.defaultProps = {
+    style: null,
+    className: []
 };
 
