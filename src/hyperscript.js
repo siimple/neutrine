b3835f688337395c749f45588f7acad65a6115f8
jmjuanes <josemijuanes@gmail.com>
2018-04-06 17:06:40 +0200

    Removed unused hyperscript

--- src/hyperscript.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import hyperscript from 'kofi-hyperscript';
-
-//Generate the react hyperscript wrapper
-var h = hyperscript(React.createElement, { });
-
-//Export the hyperscript wrapper
-export default h;
