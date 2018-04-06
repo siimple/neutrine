9cb070df2b2330b634def8e0bbfc33fc5bae6149
jmjuanes <josemijuanes@gmail.com>
2018-04-06 11:54:30 +0200

    Big rewrite

--- src/layout/jumbotron.js
+++ src/layout/jumbotron.js
@@ -1,60 +1,47 @@
-import h from '../hyperscript.js';
-import colors from '../colors.js';
-import SiimpleComponent from '../index.js';
+import React from "react";
+import {hyperscript as h} from "neutrine-utils";
 
-import sizes from './sizes.js';
+import "siimple/scss/layout/_jumbotron.scss";
 
 //Jumbotron base component
-export default class SiimpleJumbotron extends SiimpleComponent
-{
-  //Render the jumbotron component
-  render()
-  {
+export default class Jumbotron extends React.Component {
+  render() {
     //Initialize the jumbotron class list
-    var class_list = [ 'siimple-jumbotron' ];
-
+    let classList = ["siimple-jumbotron"];
     //Check the jumbotron color
-    if(typeof this.props.color === 'string' && colors.list.indexOf(this.props.color.toLowerCase()) !== -1)
-    {
-      //Add the color class
-      class_list.push('siimple-jumbotron--' + this.props.color.toLowerCase());
+    if (typeof this.props.color === "string") {
+      classList.push("siimple-jumbotron--" + this.props.color.toLowerCase());
     }
-
     //Check the jumbotron size property
-    if(typeof this.props.size === 'string' && sizes.indexOf(this.props.size.toLowerCase()) !== -1)
-    {
-      //Add the jumbotron size class
-      class_list.push('siimple-jumbotron--' + this.props.size.toLowerCase());
+    if (typeof this.props.size === "string") {
+      classList.push("siimple-jumbotron--" + this.props.size.toLowerCase());
     }
-
     //Initialize the jumbotron children array
-    var children = [];
-
+    let children = [];
     //Check the title property
-    if(typeof this.props.title === 'string')
-    {
-      //Add the title element
-      children.push(h.div({ className: 'siimple-jumbotron-title' }, this.props.title));
+    if (typeof this.props.title === "string") {
+      children.push(h.div({ className: "siimple-jumbotron-title" }, this.props.title));
     }
-
     //Check the subtitle property
-    if(typeof this.props.subtitle === 'string')
-    {
-      //Add the subtitle element
-      children.push(h.div({ className: 'siimple-jumbotron-subtitle' }, this.props.subtitle));
+    if (typeof this.props.subtitle === "string") {
+      children.push(h.div({ className: "siimple-jumbotron-subtitle" }, this.props.subtitle));
     }
-
     //Check the detail property
-    if(typeof this.props.detail === 'string')
-    {
-      //Add the detail element
-      children.push(h.div({ className: 'siimple-jumbotron-detail' }, this.props.detail));
+    if (typeof this.props.detail === "string") {
+      children.push(h.div({ className: "siimple-jumbotron-detail" }, this.props.detail));
     }
-
     //Return the parent div
-    return h.div({ className: class_list, style: this.props.style }, children, this.props.children);
+    return h("div", {className: classList, style: this.props.style}, children, this.props.children);
   }
 }
 
 //Jumbotron default props
-SiimpleJumbotron.defaultProps = { color: null, size: null, style: {}, title: null, subtitle: null, detail: null };
+Jumbotron.defaultProps = {
+    color: null, 
+    size: null, 
+    style: null, 
+    title: null, 
+    subtitle: null, 
+    detail: null 
+};
+
