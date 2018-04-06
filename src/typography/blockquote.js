96b52a247b057abfc02df8b957215468f635952d
jmjuanes <josemijuanes@gmail.com>
2018-04-06 16:23:55 +0200

    Init blockquote component

---
+++ src/typography/blockquote.js
@@ -0,0 +1,16 @@
+import React from "react";
+import {hyperscript as h} from "neutrine-utils";
+
+import "siimple/scss/typography/_blockquote.scss";
+
+//Export bagde component 
+export default class Blockquote extends React.Component {
+    render() {
+        //Return the blockquote element
+        return h("div", {className: "siimple-blockquote", style: this.props.style}, this.props.children);
+    }
+}
+
+//Blockquote default props
+Blockquote.defaultProps = {style: null};
+
