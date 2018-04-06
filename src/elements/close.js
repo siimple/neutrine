20f7a2e138ba40fdadc563ceec6493137abda51d
jmjuanes <josemijuanes@gmail.com>
2018-04-06 18:17:03 +0200

    Minor changes

--- src/elements/close.js
+++ src/elements/close.js
@@ -1,21 +1,21 @@
 import React from "react";
 import {hyperscript as h} from "neutrine-utils";
+import {omit} from "kofi";
 
 import "siimple/scss/elements/_close.scss";
 
 //Close button class
 export default class Close extends React.Component {
     render() {
-        let self = this;
-        let classList = ["siimple-close"];
-        return h("div", {className: classList, onClick: self.props.onClick, style: self.props.style});
+        //Clone the close button props
+        let props = omit(this.props, ["children", "className"]);
+        //Add the close button class name
+        props.className = ["siimple-close"];
+        //Return the close button
+        return h("div", props, null);
     }
 }
 
 //Default props
-Close.defaultProps = { 
-    size: null, 
-    style: null, 
-    onClick: null 
-};
+Close.defaultProps = {style: null};
 
