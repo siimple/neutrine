151463f386c652e3afb6456fcbebcccb15173a92
jmjuanes <josemijuanes@gmail.com>
2018-04-06 11:09:14 +0200

    Minor code reformat. Fixed imports

--- src/grid/grid.js
+++ src/grid/grid.js
@@ -1,48 +1,53 @@
-import SiimpleComponent from '../index.js';
-import h from '../hyperscript.js';
+import React from "react";
+import {hyperscript as h} from "neutrine-utils";
 
 //Grid class
-export class SiimpleGrid extends SiimpleComponent {
+export class Grid extends React.Component {
     render() {
-        return h('div', {className: 'siimple-grid', style: this.props.style}, this.props.children);
+        return h("div", {className: "siimple-grid", style: this.props.style}, this.props.children);
     }
 }
 
 //Grid default props
-SiimpleGrid.defaultProps = { style: null };
+Grid.defaultProps = {style: null};
 
 //Grid row class
-export class SiimpleGridRow extends SiimpleComponent {
+export class GridRow extends React.Component {
     render() {
-        return h('div', {className: 'siimple-grid-row', style: this.props.style}, this.props.children);
+        return h("div", {className: "siimple-grid-row", style: this.props.style}, this.props.children);
     }
 }
 
 //Grid row default props
-SiimpleGridRow.defaultProps = { style: null };
+GridRow.defaultProps = {style: null};
 
 //Grid column class
-export class SiimpleGridCol extends SiimpleComponent {
+export class GridCol extends React.Component {
     render() {
-        var class_list = ['siimple-grid-col'];
-
+        let classList = ["siimple-grid-col"];
         //Check the column size
-        if (typeof this.props.size === 'number') {
-            class_list.push('siimple-grid-col--' + this.props.size);
+        if (typeof this.props.size === "number") {
+            classList.push("siimple-grid-col--" + this.props.size);
         }
-        if (typeof this.props.large === 'number') {
-            class_list.push('siimple-grid-col-lg--' + this.props.large);
+        if (typeof this.props.large === "number") {
+            classList.push("siimple-grid-col-lg--" + this.props.large);
         }
-        if (typeof this.props.medium === 'number') {
-            class_list.push('siimple-grid-col-md--' + this.props.medium);
+        if (typeof this.props.medium === "number") {
+            classList.push("siimple-grid-col-md--" + this.props.medium);
         }
-        if (typeof this.props.small === 'number') {
-            class_list.push('siimple-grid-col-sm--' + this.props.medium);
+        if (typeof this.props.small === "number") {
+            classList.push("siimple-grid-col-sm--" + this.props.medium);
         }
-
-        return h('div', {className: class_list, style: this.props.style}, this.props.children);
+        return h("div", {className: classList, style: this.props.style}, this.props.children);
     }
 }
 
 //Column default props
-SiimpleGridCol.defaultProps = {size: null, large: null, medium: null, small: null, style: null};
+GridCol.defaultProps = {
+    size: null, 
+    large: null, 
+    medium: null, 
+    small: null, 
+    style: null
+};
+
