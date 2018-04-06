4910055fc7215103643b7d8c614ce5b5b9d0fa3e
jmjuanes <josemijuanes@gmail.com>
2018-04-06 13:11:10 +0200

    Big rewrite. Added box title, subtitle and detail components

--- src/layout/box.js
+++ src/layout/box.js
@@ -1,42 +1,73 @@
 import React from "react";
 import {hyperscript as h} from "neutrine-utils";
+import {concat, omit} from "kofi";
 
 import "siimple/scss/layout/_box.scss";
 
 //Box layout class
-export default class Box extends React.Component {
+export class Box extends React.Component {
     render() {
-        //Initialize the box class list
-        let classList = ["siimple-box"];
+        //Clone the box props 
+        let props = omit(this.props, ["children", "className", "color"]);
+        props.className = concat(["siimple-box"], this.props.className);
         //Check the box color property
         if (typeof this.props.color === "string") {
-            classList.push("siimple-box--" + this.props.color.toLowerCase());
-        }
-        //Initialize the box children list
-        let children = [];
-        //Check the title property
-        if (typeof this.props.title === "string") {
-            children.push(h("div", {className: "siimple-box-title"}, this.props.title));
-        }
-        //Check the subtitle property
-        if (typeof this.props.subtitle === "string") {
-            children.push(h("div", {className: "siimple-box-subtitle"}, this.props.subtitle));
-        }
-        //Check the detail property
-        if (typeof this.props.detail === "string") {
-            children.push(h("div", {className: "siimple-box-detail"}, this.props.detail));
+            props.className.push("siimple-box--" + this.props.color.toLowerCase());
         }
         //Return the box component
-        return h("div", {className: classList, style: this.props.style}, children, this.props.children);
+        return h("div", props, this.props.children);
     }
 }
 
 //Default props
 Box.defaultProps = {
     color: null, 
-    title: null, 
-    subtitle: null, 
-    detail: null,
-    style: null
+    style: null,
+    className: []
+};
+
+//Box title
+export class BoxTitle extends React.Component {
+    render() {
+        let props = omit(this.props, ["children", "className"]);
+        props.className = concat(["siimple-box-title"], this.props.className);
+        return h("div", props, this.props.children);
+    }
+}
+
+//Box title default props 
+BoxTitle.defaultProps = {
+    style: null,
+    className: []
+};
+
+//Box subtitle 
+export class BoxSubtitle extends React.Component {
+    render() {
+        let props = omit(this.props, ["children", "className"]);
+        props.className = concat(["siimple-box-subtitle"], this.props.className);
+        return h("div", props, this.props.children);
+    }
+}
+
+//Box subtitle default props 
+BoxSubtitle.defaultProps = {
+    style: null,
+    className: []
+};
+
+//Box detail component 
+export class BoxDetail extends React.Component {
+    render() {
+        let props = omit(this.props, ["children", "className"]);
+        props.className = concat(["siimple-box-detail"], this.props.className);
+        return h("div", props, this.props.children);
+    }
+}
+
+//Box detail default props 
+BoxDetail.defaultProps = {
+    style: null,
+    className: []
 };
 
