507b49add9386d1c04394724a4a0911d380c2975
jmjuanes <josemijuanes@gmail.com>
2018-04-06 11:47:36 +0200

    Big rewrite

--- src/layout/content.js
+++ src/layout/content.js
@@ -1,28 +1,25 @@
-import h from '../hyperscript.js';
-import SiimpleComponent from '../index.js';
+import React from "react";
+import {hyperscript as h} from "neutrine-utils";
 
-import sizes from './sizes.js';
+import "siimple/scss/layout/_content.scss";
 
 //Content layout component
-export default class SiimpleContent extends SiimpleComponent
-{
-  //Render the content component
-  render()
-  {
-    //Initialize the content class list
-    var class_list = [ 'siimple-content' ];
-
-    //Check the content size
-    if(typeof this.props.size === 'string' && sizes.indexOf(this.props.size.toLowerCase()) !== -1)
-    {
-      //Add the size class
-      class_list.push('siimple-content--' + this.props.size.toLowerCase());
+export default class Content extends React.Component {
+    render() {
+        //Initialize the content class list
+        let classList = ["siimple-content"];
+        //Check the content size
+        if (typeof this.props.size === "string") {
+            classList.push("siimple-content--" + this.props.size.toLowerCase());
+        }
+        //Render the content div
+        return h("div", {className: classList, style: this.props.style}, this.props.children);
     }
-
-    //Render the content div
-    return h.div({ className: class_list, style: this.props.style }, this.props.children);
-  }
 }
 
 //Default props
-SiimpleContent.defaultProps = { size: null, style: {} };
+Content.defaultProps = {
+    size: null, 
+    style: null 
+};
+
