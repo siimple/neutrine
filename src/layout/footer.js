431432a2386b6ffbe010845529bcb6fcfda4a68c
jmjuanes <josemijuanes@gmail.com>
2018-04-06 11:50:35 +0200

    Big rewrite

--- src/layout/footer.js
+++ src/layout/footer.js
@@ -1,36 +1,30 @@
-import h from '../hyperscript.js';
-import colors from '../colors.js';
-import SiimpleComponent from '../index.js';
+import React from "react";
+import {hyperscript as h} from "neutrine-utils";
 
-import sizes from './sizes.js';
+import "siimple/scss/layout/_footer.scss";
 
 //Footer layout component
-export default class SiimpleFooter extends SiimpleComponent
-{
-  //Render the footer component
-  render()
-  {
-    //Initialize the footer class list
-    var class_list = [ 'siimple-footer' ];
-
-    //Check the color
-    if(typeof this.props.color === 'string' && colors.list.indexOf(this.props.color.toLowerCase()) !== -1)
-    {
-      //Add the color class
-      class_list.push('siimple-footer--' + this.props.color.toLowerCase());
+export default class Footer extends React.Component {
+    render() {
+        //Initialize the footer class list
+        let classList = ["siimple-footer"];
+        //Check the color
+        if (typeof this.props.color === "string") {
+            classList.push("siimple-footer--" + this.props.color.toLowerCase());
+        }
+        //Check the content size
+        if (typeof this.props.size === "string") {
+            classList.push("siimple-footer--" + this.props.size.toLowerCase());
+        }
+        //Render the footer div
+        return h("div", {className: classList, style: this.props.style}, this.props.children);
     }
-
-    //Check the content size
-    if(typeof this.props.size === 'string' && sizes.indexOf(this.props.size.toLowerCase()) !== -1)
-    {
-      //Add the size class
-      class_list.push('siimple-footer--' + this.props.size.toLowerCase());
-    }
-
-    //Render the footer div
-    return h.div({ className: class_list }, this.props.children);
-  }
 }
 
 //Default props
-SiimpleFooter.defaultProps = { color: null, size: null };
+Footer.defaultProps = {
+    color: null, 
+    size: null, 
+    style: null
+};
+
