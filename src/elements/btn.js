ae447bb0c2fb6d0ef22a491d5f03f2a8a868890d
jmjuanes <josemijuanes@gmail.com>
2018-04-07 23:05:20 +0200

    Changed button default props. Minor changes

--- src/elements/btn.js
+++ src/elements/btn.js
@@ -1,36 +1,33 @@
 import React from "react";
 import {hyperscript as h} from "neutrine-utils";
+import {concat, omit} from "kofi";
 
 import "siimple/scss/elements/_btn.scss";
 
 //Button component
 export default class Btn extends React.Component {
     render() {
-        let self = this;
-        let btnProps = {
-            className: ["siimple-btn"],
-            onClick: self.props.onClick, 
-            style: {}
-        };
+        //Initialize the button props 
+        let props = omit(this.props, ["children", "className", "color", "disabled", "fluid"]);
+        //Initialize the class names list 
+        let className = ["siimple-btn"];
         //Add the button color
         if (typeof this.props.color === "string") {
-            btnProps.className.push("siimple-btn--" + this.props.color.toLowerCase().trim());
+            className.push("siimple-btn--" + this.props.color.toLowerCase().trim());
         }
         //Add the button disabled option
         if (this.props.disabled === true) {
-            btnProps.className.push("siimple-btn--disabled");
+            className.push("siimple-btn--disabled");
         }
         //Check the fluid property
         if (this.props.fluid === true) {
             //btn_props.style = { width: '100%', paddingLeft: '0px', paddingRight: '0px' };
-            btnProps.className.push("siimple-btn--fluid");
+            className.push("siimple-btn--fluid");
         }
-        //Check the custom styles
-        if (typeof this.props.style === "object" && this.props.style !== null) {
-            btnProps.style = Object.assign(btnProps.style, this.props.style);
-        }
+        //Append the provided class names
+        props.className = concat(className, this.props.className);
         //Return the button element
-        return h("div", btnProps, this.props.children);
+        return h("div", props, this.props.children);
     }
 }
 
@@ -38,8 +35,6 @@ export default class Btn extends React.Component {
 Btn.defaultProps = { 
     color: "blue", 
     disabled: false, 
-    onClick: null, 
-    style: null, 
     fluid: false 
 };
 
