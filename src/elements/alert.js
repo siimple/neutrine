047043c5329adf60f78238208f6ec82b75872821
jmjuanes <josemijuanes@gmail.com>
2018-04-07 23:53:30 +0200

    Parse class list using classNames util

--- src/elements/alert.js
+++ src/elements/alert.js
@@ -1,6 +1,6 @@
 import React from "react";
-import {hyperscript as h} from "neutrine-utils";
-import {concat, omit} from "kofi";
+import {classNames, hyperscript as h} from "neutrine-utils";
+import {omit} from "kofi";
 
 import "siimple/scss/elements/_alert.scss";
 
@@ -10,13 +10,13 @@ export class Alert extends React.Component {
         //Clone the alert props 
         let props = omit(this.props, ["children", "className", "color"]);
         //Initialize the class list
-        let className = ["siimple-alert"];
+        let classList = ["siimple-alert"];
         //Check the alert color property
         if (typeof this.props.color === "string") {
-            className.push("siimple-alert--"  + this.props.color.toLowerCase().trim());
+            classList.push("siimple-alert--"  + this.props.color.toLowerCase().trim());
         }
         //Append the provided class list 
-        props.className = concat(className, this.props.className);
+        props.className = classNames(classList, this.props.className);
         return h("div", props, this.props.children);
     }
 }
@@ -31,8 +31,8 @@ export class AlertTitle extends React.Component {
     render() {
         //Initialize the element props 
         let props = omit(this.props, ["children", "className"]);
-        //Initialize the class names list
-        props.className = concat(["siimple-alert-title"], this.props.className);
+        //Generate the class names
+        props.className = classNames(["siimple-alert-title"], this.props.className);
         //Return the alert title element
         return h("div", props, this.props.children);
     }
