3f1e5e87b2b0f1d87d2d487d0e45cbef789ede2e
jmjuanes <josemijuanes@gmail.com>
2018-04-07 23:54:19 +0200

    Minor changes

--- src/elements/btn.js
+++ src/elements/btn.js
@@ -1,6 +1,6 @@
 import React from "react";
-import {hyperscript as h} from "neutrine-utils";
-import {concat, omit} from "kofi";
+import {classNames, hyperscript as h} from "neutrine-utils";
+import {omit} from "kofi";
 
 import "siimple/scss/elements/_btn.scss";
 
@@ -10,22 +10,22 @@ export default class Btn extends React.Component {
         //Initialize the button props 
         let props = omit(this.props, ["children", "className", "color", "disabled", "fluid"]);
         //Initialize the class names list 
-        let className = ["siimple-btn"];
+        let classList = ["siimple-btn"];
         //Add the button color
         if (typeof this.props.color === "string") {
-            className.push("siimple-btn--" + this.props.color.toLowerCase().trim());
+            classList.push("siimple-btn--" + this.props.color.toLowerCase().trim());
         }
         //Add the button disabled option
         if (this.props.disabled === true) {
-            className.push("siimple-btn--disabled");
+            classList.push("siimple-btn--disabled");
         }
         //Check the fluid property
         if (this.props.fluid === true) {
             //btn_props.style = { width: '100%', paddingLeft: '0px', paddingRight: '0px' };
-            className.push("siimple-btn--fluid");
+            classList.push("siimple-btn--fluid");
         }
         //Append the provided class names
-        props.className = concat(className, this.props.className);
+        props.className = classNames(className, this.props.className);
         //Return the button element
         return h("div", props, this.props.children);
     }
