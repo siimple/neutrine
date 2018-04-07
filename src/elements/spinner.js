7eeeb61866c254e40ee5e1c6fcbb2eaaf087d622
jmjuanes <josemijuanes@gmail.com>
2018-04-07 23:21:31 +0200

    Changed spinner default dependencies. Minor changes

--- src/elements/spinner.js
+++ src/elements/spinner.js
@@ -1,11 +1,13 @@
 import React from "react";
 import {hyperscript as h} from "neutrine-utils";
+import {concat, omit}, from "kofi";
 
 import "siimple/scss/elements/_spinner.scss";
 
 //Spinner class
 export default class Spinner extends React.Component {
     render() {
+        let props = omit(this.props, ["children", "className", "color", "size"]);
         let classList = ["siimple-spinner"];
         //Check the color attribute
         if(typeof this.props.color === "string") {
@@ -15,15 +17,15 @@ export default class Spinner extends React.Component {
         if (typeof this.props.size === "string") {
             classList.push("siimple-spinner--" + this.props.size);
         }
+        props.className = concat(classList, this.props.className);
         //Return the spinner element
-        return h("div", {className: classList, style: this.props.style}, null);
+        return h("div", props, null);
     }
 }
 
 //Spinner default props
 Spinner.defaultProps = { 
     color: "blue", 
-    size: null, 
-    style: null 
+    size: null
 };
 
