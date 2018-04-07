8c45c6f1df88f6e2931fa4051d4276c832a0a84b
jmjuanes <josemijuanes@gmail.com>
2018-04-07 23:17:50 +0200

    Fixed list default props. Minor changes

--- src/elements/list.js
+++ src/elements/list.js
@@ -1,43 +1,53 @@
 import React from "react";
 import {hyperscript as h} from "neutrine-utils";
+import {concat, omit} from "kofi";
 
 import "siimple/scss/elements/_list.scss";
 
 //List component 
 export class List extends React.Component {
     render () {
+        //Initialize the list props
+        let props = omit(this.props, ["children", "className", "hover"]);
         let classList = ["siimple-list"];
         //Check the hover prop
         if (this.props.hover === true) {
             classList.push("siimple-list--hover");
         }
-        return h("div", {className: classList, style: this.props.style}, this.props.children);
+        props.className = concat(classList, this.props.className);
+        //Return the list element
+        return h("div", props, this.props.children);
     }
 }
 
 //List default props 
 List.defaultProps = {
-    hover: false,
-    style: null
+    hover: false
 };
 
 //List item component 
 export class ListItem extends React.Component {
     render() {
-        return h("div", {className: "siimple-list-item", style: this.props.style}, this.props.children);
+        //Initialize the list item props
+        let props = omit(this.props, ["children", "className"]);
+        props.className = concat(["siimple-list-item"], this.props.className);
+        //Return the list item element
+        return h("div", props, this.props.children);
     }
 }
 
 //List item default props 
-ListItem.defaultProps = {style: null};
+ListItem.defaultProps = {};
 
 //List title component 
 export class ListTitle extends React.Component {
     render() {
-        return h("div", {className: "siimple-list-title", style: this.props.style}, this.props.children);
+        let props = omit(this.props, ["children", "className"]);
+        props.className = concat(["siimple-list-title"], this.props.className);
+        return h("div", props, this.props.children);
     }
 }
 
 //List title default props 
-ListTitle.defaultProps = {style: null};
+ListTitle.defaultProps = {};
 
