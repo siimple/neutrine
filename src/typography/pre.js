6f8171a698801d21f8389b50cca43d3da8e37043
jmjuanes <josemijuanes@gmail.com>
2018-04-09 18:56:41 +0200

    Minor changes

--- src/typography/pre.js
+++ src/typography/pre.js
@@ -1,16 +1,22 @@
 import React from "react";
-import {hyperscript as h} from "neutrine-utils";
+import {classNames, hyperscript as h} from "neutrine-utils";
+import {omit} from "kofi";
 
 import "siimple/scss/typography/_pre.scss";
 
 //Export pre component 
 export default class Pre extends React.Component {
     render() {
+        //Clone the pre props
+        let props = omit(this.props, ["children", "className"]);
+        props.className = classNames("siimple-pre", this.props.className);
         //Return the pre element
-        return h("pre", {className: "siimple-pre", style: this.props.style}, this.props.children);
+        return h("pre", props, this.props.children);
     }
 }
 
 //Pre default props
-Pre.defaultProps = {style: null};
+Pre.defaultProps = {
+    style: null
+};
 
