74fa75e4c91a6f7cabba942e624110d62aee38f7
jmjuanes <josemijuanes@gmail.com>
2018-04-09 18:36:07 +0200

    Minor changes on menu components

--- src/layout/menu.js
+++ src/layout/menu.js
@@ -1,43 +1,54 @@
 import React from "react";
-import {hyperscript as h} from "neutrine-utils";
-import {concat, omit} from "kofi";
+import {classNames, hyperscript as h} from "neutrine-utils";
+import {omit} from "kofi";
 
 import "siimple/scss/layout/_menu.scss";
 
 //Menu component
 export class Menu extends React.Component {
     render() {
-        return h("div", {className: "siimple-menu", style: this.props.style}, this.props.children);
+        //Clone the menu props
+        let props = omit(this.props, ["children", "className"]);
+        props.className = classNames("siimple-menu", this.props.className);
+        //Return the menu element
+        return h("div", props, this.props.children);
     }
 }
 
 //Main menu props
-Menu.defaultProps = {style: null};
+Menu.defaultProps = {
+    style: null
+};
 
 //Menu group component
 export class MenuGroup extends React.Component {
     render() {
         //Clone the properties 
         let props = omit(this.props, ["children", "className"]);
-        props.className = "siimple-menu-group";
+        props.className = classNames("siimple-menu-group", this.props.className);
         //Return the menu group element
         return h("div", props, this.props.children);
     }
 }
 
 //Menu group default props
-MenuGroup.defaultProps = {style: null};
+MenuGroup.defaultProps = {
+    style: null
+};
 
 //Menu item
 export class MenuItem extends React.Component {
     render() {
         //Clone the properties 
         let props = omit(this.props, ["selected", "children", "className"]);
-        props.className = concat(["siimple-menu-item"], this.props.className);
+        //Initialize the menu item class list
+        let classList = ["siimple-menu-item"];
         //Check the selected attribute
         if (this.props.selected === true) {
-            props.className.push("siimple-menu-item--selected");
+            classList.push("siimple-menu-item--selected");
         }
+        //Generate the menu item classname
+        props.className = classNames(classList, this.props.className);
         //Return the menu item element
         return h("div", props, this.props.children);
     }
@@ -45,6 +56,5 @@ export class MenuItem extends React.Component {
 
 //Menu item default props
 MenuItem.defaultProps = {
-    style: null,
-    className: []
+    style: null
 };
