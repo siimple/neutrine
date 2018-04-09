5e1a7bbc6496697b88aedce1025e7cd8333816c3
jmjuanes <josemijuanes@gmail.com>
2018-04-09 18:49:09 +0200

    Minor changes

--- src/typography/blockquote.js
+++ src/typography/blockquote.js
@@ -1,16 +1,22 @@
 import React from "react";
-import {hyperscript as h} from "neutrine-utils";
+import {classNames, hyperscript as h} from "neutrine-utils";
+import {omit} from "kofi";
 
 import "siimple/scss/typography/_blockquote.scss";
 
 //Export blockquote component 
 export default class Blockquote extends React.Component {
     render() {
+        //Clone the blockquote props
+        let props = omit(this.props, ["children", "className"]);
+        props.className = classNames("siimple-blockquote", this.props.className);
         //Return the blockquote element
-        return h("div", {className: "siimple-blockquote", style: this.props.style}, this.props.children);
+        return h("div", props, this.props.children);
     }
 }
 
 //Blockquote default props
-Blockquote.defaultProps = {style: null};
+Blockquote.defaultProps = {
+    style: null
+};
 
