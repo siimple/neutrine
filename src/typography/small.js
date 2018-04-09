6410354ff7a28e782cffb115b924e13de25ad89e
jmjuanes <josemijuanes@gmail.com>
2018-04-09 18:58:27 +0200

    Minor changes

--- src/typography/small.js
+++ src/typography/small.js
@@ -1,16 +1,22 @@
 import React from "react";
-import {hyperscript as h} from "neutrine-utils";
+import {classNames, hyperscript as h} from "neutrine-utils";
+import {omit} from "kofi";
 
 import "siimple/scss/typography/_small.scss";
 
 //Export small component 
 export default class Small extends React.Component {
     render() {
+        //Clone the small props
+        let props = omit(this.props, ["children", "className"]);
+        props.className = classNames("siimple-small", this.props.className);
         //Return the small element
-        return h("span", {className: "siimple-small", style: this.props.style}, this.props.children);
+        return h("span", props, this.props.children);
     }
 }
 
 //Small default props
-Small.defaultProps = {style: null};
+Small.defaultProps = {
+    style: null
+};
 
