56d63c25b5b6ef737fdc12db851ac200555990b0
jmjuanes <josemijuanes@gmail.com>
2018-04-09 17:47:19 +0200

    Accept custom classnames

--- src/layout/content.js
+++ src/layout/content.js
@@ -1,19 +1,24 @@
 import React from "react";
-import {hyperscript as h} from "neutrine-utils";
+import {classNames, hyperscript as h} from "neutrine-utils";
+import {omit} from "kofi";
 
 import "siimple/scss/layout/_content.scss";
 
 //Content layout component
 export default class Content extends React.Component {
     render() {
+        //Content props
+        let props = omit(this.props, ["children", "className", "size"]);
         //Initialize the content class list
         let classList = ["siimple-content"];
         //Check the content size
         if (typeof this.props.size === "string") {
             classList.push("siimple-content--" + this.props.size.toLowerCase());
         }
+        //Generate the content className
+        props.className = classNames(classList, this.props.className);
         //Render the content div
-        return h("div", {className: classList, style: this.props.style}, this.props.children);
+        return h("div", props, this.props.children);
     }
 }
 
