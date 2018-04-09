01fdfa4cdc82db27d9202a4dc0a821357607107f
jmjuanes <josemijuanes@gmail.com>
2018-04-09 17:31:07 +0200

    Minor changes

--- src/form/select.js
+++ src/form/select.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {hyperscript as h} from "neutrine-utils";
+import {classNames, hyperscript as h} from "neutrine-utils";
 import {omit} from "kofi";
 
 import "siimple/scss/form/_select.scss";
@@ -8,13 +8,15 @@ import "siimple/scss/form/_select.scss";
 export default class Select extends React.Component {
     render() {
         //Clone the select props
-        let props = omit(this.props, ["fluid", "children"])
-        //Initialize the select class
-        props.className = ["siimple-select"];
+        let props = omit(this.props, ["fluid", "children", "className"])
+        //Initialize the select class list
+        let classList = ["siimple-select"];
         //Check the fluid property
         if (this.props.fluid === true) {
             props.className.push("siimple-select--fluid");
         }
+        //Save the className
+        props.className = classNames(classList, this.props.className);
         //Return the select element
         return h("select", props, this.props.children);
     }
