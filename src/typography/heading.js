bdb9ef9c638137044a1cc70f9b1aced0879d827f
jmjuanes <josemijuanes@gmail.com>
2018-04-09 18:52:31 +0200

    Minor changes

--- src/typography/heading.js
+++ src/typography/heading.js
@@ -1,6 +1,6 @@
 import React from "react";
-import {hyperscript as h} from "neutrine-utils";
-import {concat, omit} from "kofi";
+import {classNames, hyperscript as h} from "neutrine-utils";
+import {omit} from "kofi";
 
 import "siimple/scss/typography/_heading.scss";
 
@@ -9,12 +9,14 @@ export default class Heading extends React.Component {
     render() {
         //Initialize the header props
         let props = omit(this.props, ["children", "type", "className"]);
-        //Initialize the header class names
-        props.className = concat([], this.props.className);
+        //Initialize the header class list
+        let classList = [];
         //Check the header type
         if (typeof this.props.type === "string" && this.props.type.charAt(0).toLowerCase() === "h" && this.props.type.length === 2) {
-            props.className.push("siimple-" + this.props.type.toLowerCase().trim());
+            classList.push("siimple-" + this.props.type.toLowerCase().trim());
         }
+        //Generate the header classname
+        props.className = classNames(classList, this.props.className);
         //Return the heading element
         return h("div", props, this.props.children);
     }
@@ -23,7 +25,6 @@ export default class Heading extends React.Component {
 //Default heading props
 Heading.defaultProps = {
     type: "h1",
-    style: null,
-    className: []
+    style: null
 };
 
