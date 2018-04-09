df21c9aa936533f6b82366d2c7d0a54627d55a77
jmjuanes <josemijuanes@gmail.com>
2018-04-09 18:15:36 +0200

    Minor changes on footer component

--- src/layout/footer.js
+++ src/layout/footer.js
@@ -1,11 +1,14 @@
 import React from "react";
-import {hyperscript as h} from "neutrine-utils";
+import {classNames, hyperscript as h} from "neutrine-utils";
+import {omit} from "kofi";
 
 import "siimple/scss/layout/_footer.scss";
 
 //Footer layout component
 export default class Footer extends React.Component {
     render() {
+        //Clone the footer props 
+        let props = omit(this.props, ["children", "className", "color", "size"]);
         //Initialize the footer class list
         let classList = ["siimple-footer"];
         //Check the color
@@ -16,8 +19,10 @@ export default class Footer extends React.Component {
         if (typeof this.props.size === "string") {
             classList.push("siimple-footer--" + this.props.size.toLowerCase());
         }
+        //Generate the footer classname
+        props.className = classNames(classList, this.props.className);
         //Render the footer div
-        return h("div", {className: classList, style: this.props.style}, this.props.children);
+        return h("div", props, this.props.children);
     }
 }
 
