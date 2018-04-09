b26bf965f1dd4cfb8d9b62a66145c89cba32add8
jmjuanes <josemijuanes@gmail.com>
2018-04-09 19:00:20 +0200

    Minor changes

--- src/typography/tag.js
+++ src/typography/tag.js
@@ -1,19 +1,24 @@
 import React from "react";
-import {hyperscript as h} from "neutrine-utils";
+import {classNames, hyperscript as h} from "neutrine-utils";
+import {omit} from "kofi";
 
 import "siimple/scss/typography/_tag.scss";
 
 //Export tag component 
 export default class Tag extends React.Component {
     render() {
+        //Clone the tag component props
+        let props = omit(this.props, ["children", "className", "color"]);
         //Initialize the tag class list 
         let classList = ["siimple-tag"];
         //Check the color attribute
         if (typeof this.props.color === "string") {
             classList.push("siimple-tag--" + this.props.color.toLowerCase().trim());
         }
+        //Generate the tag classname
+        props.className = classNames(classList, this.props.className);
         //Return the tag element
-        return h("div", {className: classList, style: this.props.style}, this.props.children);
+        return h("div", props, this.props.children);
     }
 }
 
