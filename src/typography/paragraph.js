7a661a0aaa7fc6e1200f45d1eaf4d17a59a74c33
jmjuanes <josemijuanes@gmail.com>
2018-04-09 18:54:15 +0200

    Minor code reformat. Clone paragraph props and add the paragraph class

--- src/typography/paragraph.js
+++ src/typography/paragraph.js
@@ -1,16 +1,22 @@
 import React from "react";
-import {hyperscript as h} from "neutrine-utils";
+import {classNames, hyperscript as h} from "neutrine-utils";
+import {omit} from "kofi";
 
 import "siimple/scss/typography/_paragraph.scss";
 
 //Paragraph component
 export default class Paragraph extends React.Component {
-  render() {
-    //Return a div with the paragraph class
-    return h("div", {className: "siimple-paragraph", style: this.props.style}, this.props.children);
-  }
+    render() {
+        //Clone the paragraph props
+        let props = omit(this.props, ["children", "className"]);
+        props.className = classNames("siimple-paragraph", this.props.className);
+        //Return a div with the paragraph class
+        return h("div", props, this.props.children);
+    }
 }
 
 //Paragraph default props
-Paragraph.defaultProps = {style: null};
+Paragraph.defaultProps = {
+    style: null
+};
 
