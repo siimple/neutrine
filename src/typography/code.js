5d86d888ffc80a428911d70a824daa7f221275fe
jmjuanes <josemijuanes@gmail.com>
2018-04-09 18:51:07 +0200

    Minor changes on code component

--- src/typography/code.js
+++ src/typography/code.js
@@ -1,16 +1,22 @@
 import React from "react";
-import {hyperscript as h} from "neutrine-utils";
+import {classNames, hyperscript as h} from "neutrine-utils";
+import {omit} from "kofi";
 
 import "siimple/scss/typography/_code.scss";
 
 //Export code component 
 export default class Code extends React.Component {
     render() {
+        //Clone the code props
+        let props = omit(this.props, ["children", "className"]);
+        props.className = classNames("siimple-code", this.props.className);
         //Return the code element
-        return h("code", {className: "siimple-code", style: this.props.style}, this.props.children);
+        return h("code", props, this.props.children);
     }
 }
 
 //Code default props
-Code.defaultProps = {style: null};
+Code.defaultProps = {
+    style: null
+};
 
