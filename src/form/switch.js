eeedfcd609714d78751b63e56292f37a27bdd951
jmjuanes <josemijuanes@gmail.com>
2018-04-09 17:32:39 +0200

    Minor changes

--- src/form/switch.js
+++ src/form/switch.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {hyperscript as h} from "neutrine-utils";
+import {classNames, hyperscript as h} from "neutrine-utils";
 import {omit, uniqueId} from "kofi";
 
 import "siimple/scss/form/_switch.scss";
@@ -13,7 +13,7 @@ export default class Switch extends React.Component {
 
     render() {
         //Input default props
-        let inputProps = omit(this.props, ["style", "id"]);
+        let inputProps = omit(this.props, ["style", "id", "children", "className"]);
         inputProps.type = "checkbox";
         inputProps.id = (typeof this.props.id === "string") ? this.props.id : this.id;
         //Switch children content
@@ -22,11 +22,15 @@ export default class Switch extends React.Component {
             h("label", {htmlFor: inputProps.id}, null),
             h("div", {}, null)
         ];
+        //Generate the switch className
+        let className = classNames("siimple-switch", this.props.className);
         //Return the switch element
-        return h("div", {className: "siimple-switch", style: this.props.style}, children);
+        return h("div", {className: className, style: this.props.style}, children);
     }
 }
 
 //Switch default props
-Switch.defaultProps = {style: null};
+Switch.defaultProps = {
+    style: null
+};
 
