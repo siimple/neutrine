314743eaca04f82908c696f2cad723eef25859e4
jmjuanes <josemijuanes@gmail.com>
2018-04-09 18:47:38 +0200

    Extend props in badge component. Minor changes

--- src/typography/badge.js
+++ src/typography/badge.js
@@ -1,19 +1,24 @@
 import React from "react";
-import {hyperscript as h} from "neutrine-utils";
+import {classNames, hyperscript as h} from "neutrine-utils";
+import {omit} from "kofi";
 
 import "siimple/scss/typography/_badge.scss";
 
 //Export bagde component 
 export default class Badge extends React.Component {
     render() {
+        //Clone the badge props 
+        let props = omit(this.props, ["className", "children", "color"]);
         //Initialize the badge class list 
         let classList = ["siimple-badge"];
         //Check the color attribute
         if (typeof this.props.color === "string") {
             classList.push("siimple-badge--" + this.props.color.toLowerCase().trim());
         }
+        //Generate the badge classname
+        props.className = classNames(classList, this.props.className);
         //Return the badge element
-        return h("div", {className: classList, style: this.props.style}, this.props.children);
+        return h("div", props, this.props.children);
     }
 }
 
