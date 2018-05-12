004ba03b61a268f852e386c85bcc798cd2e9d4c5
jmjuanes <josemijuanes@gmail.com>
2018-05-12 19:00:55 +0200

    Moved to components folder. Minor changes

--- src/components/table.js
+++ src/components/table.js
@@ -1,13 +1,13 @@
 import React from "react";
-import {classNames, hyperscript as h} from "neutrine-utils";
-import {omit} from "kofi";
+import classNames from "../class-names.js";
+import getProps from "../get-props.js";
 
 import "siimple/scss/elements/_table.scss";
 
 //Export main table component
 export class Table extends React.Component {
     render() {
-        let props = omit(this.props, ["children", "className", "striped", "border", "hover"]);
+        let props = getProps(this.props, ["className", "striped", "border", "hover"]);
         let classList = ["siimple-table"];
         //Check the striped property
         if (this.props.striped === true) {
@@ -22,7 +22,7 @@ export class Table extends React.Component {
             classList.push("siimple-table--hover");
         }
         props.className = classNames(classList, this.props.className);
-        return h("div", props, this.props.children);
+        return React.createElement("div", props, this.props.children);
     }
 }
 
@@ -36,9 +36,9 @@ Table.defaultProps = {
 //Export table header component
 export class TableHeader extends React.Component {
     render() {
-        let props = omit(this.props, ["children", "className"]);
+        let props = getProps(this.props, ["className"]);
         props.className = classNames("siimple-table-header", this.props.className);
-        return h("div", props, this.props.children);
+        return React.createElement("div", props, this.props.children);
     }
 }
 
@@ -48,9 +48,9 @@ TableHeader.defaultProps = {};
 //Export table body component
 export class TableBody extends React.Component {
     render() {
-        let props = omit(this.props, ["children", "className"]);
+        let props = getProps(this.props, ["className"]);
         props.className = classNames("siimple-table-body", this.props.className);
-        return h("div", {className: "siimple-table-body", style: this.props.style}, this.props.children);
+        return React.createElement("div", {className: "siimple-table-body", style: this.props.style}, this.props.children);
     }
 }
 
@@ -60,9 +60,9 @@ TableBody.defaultProps = {};
 //Export table row component
 export class TableRow extends React.Component {
     render() {
-        let props = omit(this.props, ["children", "className"]);
+        let props = getProps(this.props, ["className"]);
         props.className = classNames("siimple-table-row", this.props.className); 
-        return h("div", props, this.props.children);
+        return React.createElement("div", props, this.props.children);
     }
 }
 
@@ -72,9 +72,9 @@ TableRow.defaultProps = {};
 //Export table cell component
 export class TableCell extends React.Component {
     render() {
-        let props = omit(this.props, ["children", "className"]);
+        let props = getProps(this.props, ["className"]);
         props.className = classNames("siimple-table-cell", this.props.className);
-        return h("div", props, this.props.children);
+        return React.createElement("div", props, this.props.children);
     }
 }
 
