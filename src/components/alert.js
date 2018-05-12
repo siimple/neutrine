cbd441d8d97aaea4cb6072f4cd025e01037cf040
jmjuanes <josemijuanes@gmail.com>
2018-05-12 18:47:38 +0200

    Fixed imports. Minor changes

--- src/components/alert.js
+++ src/components/alert.js
@@ -1,6 +1,6 @@
 import React from "react";
-import {classNames, hyperscript as h} from "neutrine-utils";
-import {omit} from "kofi";
+import classNames from "../class-names.js";
+import getProps from "../get-props.js";
 
 import "siimple/scss/elements/_alert.scss";
 
@@ -8,7 +8,7 @@ import "siimple/scss/elements/_alert.scss";
 export class Alert extends React.Component {
     render() {
         //Clone the alert props 
-        let props = omit(this.props, ["children", "className", "color"]);
+        let props = getProps(this.props, ["className", "color"]);
         //Initialize the class list
         let classList = ["siimple-alert"];
         //Check the alert color property
@@ -17,7 +17,7 @@ export class Alert extends React.Component {
         }
         //Append the provided class list 
         props.className = classNames(classList, this.props.className);
-        return h("div", props, this.props.children);
+        return React.createElement("div", props, this.props.children);
     }
 }
 
@@ -30,11 +30,11 @@ Alert.defaultProps = {
 export class AlertTitle extends React.Component {
     render() {
         //Initialize the element props 
-        let props = omit(this.props, ["children", "className"]);
+        let props = getProps(this.props, ["className"]);
         //Generate the class names
         props.className = classNames(["siimple-alert-title"], this.props.className);
         //Return the alert title element
-        return h("div", props, this.props.children);
+        return React.createElement("div", props, this.props.children);
     }
 }
 
