6dede4a58a016b1ab2391611e7a4dd7970c33986
jmjuanes <josemijuanes@gmail.com>
2018-05-13 14:21:25 +0200

    Forward radio element ref to generated input element

--- src/form/radio.js
+++ src/form/radio.js
@@ -1,40 +1,33 @@
 import React from "react";
-import {classNames, hyperscript as h} from "neutrine-utils";
-import {omit, uniqueId} from "kofi";
+import classNames from "../class-names.js";
+import getProps from "../get-props.js";
 
 import "siimple/scss/form/_radio.scss";
 
-//Radio component 
-export default class Radio extends React.Component {
-    constructor(props) {
-        super(props);
-        this.id = uniqueId();
-    }
-    
-    render() {
-        //Switch input default props
-        let inputProps = omit(this.props, ["radioRef", "children", "className", "style", "id"]);
-        inputProps.type = "radio";
-        inputProps.id = (typeof this.props.id === "string") ? this.props.id : this.id;
-        //Check the radio ref property
-        if (this.props.radioRef) {
-            inputProps.ref = this.props.radioRef;
-        }
-        //Switch children content
-        let children = [
-            h("input", inputProps, null),
-            h("label", {htmlFor: inputProps.id}, null)
-        ];
-        //Radio class name
-        let className = classNames("siimple-radio", this.props.className);
-        //Return the radio element
-        return h("div", {className: className, style: this.props.style}, children);
-    } 
-}
-
-//Radio component default props
-Radio.defaultProps = {
-    radioRef: null,
-    style: null
+//Generate a random id
+let randomId = function () {
+    return Math.random().troString(36).slice(2, 15);
 };
+
+//Radio component 
+export default Radio = React.forwardRef(function (props, ref) {
+    //Switch input default props
+    let inputProps = getProps(props, ["className", "style", "id"]);
+    inputProps.type = "radio";
+    inputProps.id = (typeof props.id === "string") ? props.id : randomId;
+    //Check the radio ref property
+    if (ref) {
+        inputProps.ref = ref;
+    }
+    //Switch children content
+    let inputChild = React.createElement("input", inputProps, null);
+    let labelChild = React.createElement("label", {htmlFor: inputProps.id}, null);
+    //Radio props
+    let radioProps = {
+        className: classNames("siimple-radio", props.className),
+        style: props.style
+    };
+    //Return the radio element
+    return React.createElement("div", radioProps, children);
+});
 
