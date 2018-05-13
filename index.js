64fae4fe1921cf01709520a785c929983647c660
jmjuanes <josemijuanes@gmail.com>
2018-05-13 18:07:33 +0200

    Updated imports. Added new elements.

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
+//Components
+import {Alert, AlertTitle} from "./src/components/alert.js";
+import {Breadcrumb, BreadcrumbItem} from "./src/components/breadcrumb.js";
+import {Card, CardHeader, CardBody, CardFooter, CardTitle, CardSubtitle} from "./src/components/card.js";
+import {List, ListItem, ListTitle} from "./src/components/list.js";
+import {Menu, MenuGroup, MenuTitle} from "./src/components/menu.js";
+import {Table, TableHeader, TableBody, TableRow, TableCell} from "./src/components/table.js";
+import {Tabs, TabsItem} from "./src/components/tabs.js";
+
 //Elements
-import {Alert, AlertTitle} from "./src/elements/alert.js";
+import Badge from "./src/elements/badge.js";
 import Btn from "./src/elements/btn.js";
-import {Card, CardHeader, CardBody, CardFooter, CardTitle, CardSubtitle} from "./src/elements/card.js";
 import Close from "./src/elements/close.js";
-import {List, ListItem, ListTitle} from "./src/elements/list.js";
-import Progress from "./src/elements/progress.js";
+import Rule from "./src/components/rule.js";
 import Spinner from "./src/elements/spinner.js";
-import {Table, TableHeader, TableBody, TableRow, TableCell} from "./src/elements/table.js";
+import Tag from "./src/elements/tag.js";
 import Tip from "./src/elements/tip.js";
 
 //Form
@@ -25,16 +32,12 @@ import {Grid, GridRow, GridCol} from "./src/grid/grid.js";
 
 //Layout
 import {Box, BoxTitle, BoxSubtitle, BoxDetail} from "./src/layout/box.js";
-import {Breadcrumb, BreadcrumbItem} from "./src/layout/breadcrumb.js";
 import Content from "./src/layout/content.js";
 import Footer from "./src/layout/footer.js";
 import {Jumbotron, JumbotronTitle, JumbotronSubtitle, JumbotronDetail} from "./src/layout/jumbotron.js";
-import {Menu, MenuGroup, MenuItem} from "./src/layout/menu.js";
 import {Navbar, NavbarTitle, NavbarSubtitle, NavbarItem} from "./src/layout/navbar.js";
-import {Tabs, TabsItem} from "./src/layout/tabs.js";
 
 //Typography
-import Badge from "./src/typography/badge.js";
 import Blockquote from "./src/typography/blockquote.js";
 import Code from "./src/typography/code.js";
 import Heading from "./src/typography/heading.js";
@@ -42,29 +45,38 @@ import Link from "./src/typography/link.js";
 import Paragraph from "./src/typography/paragraph.js";
 import Pre from "./src/typography/pre.js";
 import Small from "./src/typography/small.js";
-import Tag from "./src/typography/tag.js";
 
 export {
     Alert,
     AlertTitle,
-    Btn,
+    Breadcrumb,
+    BreadcrumbItem,
     Card,
     CardHeader,
     CardBody,
     CardFooter,
     CardTitle,
     CardSubtitle,
-    Close,
     List,
     ListItem,
     ListTitle,
-    Progress,
-    Spinner,
+    Menu,
+    MenuGroup,
+    MenuItem,
     Table,
     TableHeader,
     TableBody,
     TableRow,
     TableCell,
+    Tabs,
+    TabsItem,
+
+    Badge,
+    Btn
+    Close,
+    Rule,
+    Spinner,
+    Tag,
     Tip,
     
     Checkbox,
@@ -90,32 +102,23 @@ export {
     BoxTitle,
     BoxSubtitle,
     BoxDetail,
-    Breadcrumb,
-    BreadcrumbItem,
     Content,
     Footer,
     Jumbotron,
     JumbotronTitle,
     JumbotronSubtitle,
     JumbotronDetail,
-    Menu,
-    MenuGroup,
-    MenuItem,
     Navbar,
     NavbarTitle,
     NavbarSubtitle,
     NavbarItem,
-    Tabs,
-    TabsItem,
 
-    Badge,
     Blockquote,
     Code,
     Heading,
     Link,
     Paragraph,
     Pre,
-    Small,
-    Tag
+    Small
 }
 
