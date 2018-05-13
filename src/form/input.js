a6423701000e1b7262fe757eb968507c0e96583a
jmjuanes <josemijuanes@gmail.com>
2018-05-13 14:04:58 +0200

    Forward ref to input element. Minor changes

--- src/form/input.js
+++ src/form/input.js
@@ -1,34 +1,26 @@
 import React from "react";
-import {classNames, hyperscript as h} from "neutrine-utils";
-import {omit} from "kofi";
+import classNames from "../class-names.js";
+import getProps from "../get-props.js";
 
 import "siimple/scss/form/_input.scss";
 
 //Input component
-export default class Input extends React.Component {
-    render() {
-        //Clone the input props
-        let props = omit(this.props, ["inputRef", "fluid", "children", "className"]);
-        //Initialize the input class list
-        let classList = ["siimple-input"];
-        //Check the fluid property
-        if (this.props.fluid === true) {
-            classList.push("siimple-input--fluid");
-        }
-        //Generate the input class name
-        props.className = classNames(classList, this.props.className);
-        //Check the input reference
-        if (this.props.inputRef) {
-            props.ref = this.props.inputRef;
-        }
-        //Return the input element
-        return h("input", props);
+export default Input = React.forwardRef(function (props, ref) {
+    //Clone the input props
+    let inputProps = getProps(props, ["fluid", "className"]);
+    //Initialize the input class list
+    let classList = ["siimple-input"];
+    //Check the fluid property
+    if (typeof props.fluid === "boolean" && props.fuild === true) {
+        classList.push("siimple-input--fluid");
     }
-}
-
-//Input default props
-Input.defaultProps = {
-    fluid: false,
-    inputRef: null
-};
+    //Generate the input class name
+    inputProps.className = classNames(classList, props.className);
+    //Check the input reference
+    if (ref) {
+        inputProps.ref = ref;
+    }
+    //Return the input element
+    return React.createElement("input", props);
+});
 
