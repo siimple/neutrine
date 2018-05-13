37b3323c4d967263f995f7f80ea49dbd60a01a8a
jmjuanes <josemijuanes@gmail.com>
2018-05-13 15:42:37 +0200

    Minor changes. Fixed imports

--- src/typography/link.js
+++ src/typography/link.js
@@ -1,6 +1,6 @@
 import React from "react";
-import {classNames, hyperscript as h} from "neutrine-utils";
-import {omit} from "kofi";
+import classNames from "../class-names.js";
+import getProps from "../get-props.js";
 
 import "siimple/scss/typography/_link.scss";
 
@@ -8,10 +8,10 @@ import "siimple/scss/typography/_link.scss";
 export default class Link extends React.Component {
     render() {
         //Clone the link props
-        let props = omit(this.props, ["children", "className"]);
+        let props = getProps(this.props, ["className"]);
         props.className = classNames("siimple-link", this.props.className);
         //Return the link element 
-        return h("a", props, this.props.children);
+        return React.createElement("a", props, this.props.children);
     }
 }
 
