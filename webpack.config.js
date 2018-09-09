090148673a78d8245d26ed660d15360bb1fcd315
jmjuanes <josemijuanes@gmail.com>
2018-09-09 18:41:05 +0200

    Extract images using file-loader plugin

--- webpack.config.js
+++ webpack.config.js
@@ -36,6 +36,18 @@ module.exports = {
                         }
                     }
                 ]
+            },
+            {
+                "test": /\.(png|jpg|gif|svg)$/,
+                "use": [
+                    {
+                        "loader": "file-loader",
+                        "options": {
+                            "name": "[path][name].[ext]",
+                            "outputPath": "images/"
+                        }
+                    }
+                ]
             }
         ]
     },
