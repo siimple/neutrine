ac45b78c72652a0de1d323ca6cd870d2dfded377
jmjuanes <josemijuanes@gmail.com>
2018-10-27 19:04:27 +0200

    Fix bug in breadcrumb component. Fix components utils path. Minor changes

--- src/core/components/breadcrumb.js
+++ src/core/components/breadcrumb.js
@@ -1,7 +1,10 @@
 import React from "react";
-import classNames from "../class-names.js";
-import getProps from "../get-props.js";
 
+//Import components utils
+import classNames from "../../class-names.js";
+import getProps from "../../get-props.js";
+
+//Import breadcrumb styles
 import "siimple/scss/components/_breadcrumb.scss";
 
 //Breadcrumb parent component
@@ -15,24 +18,14 @@ export class Breadcrumb extends React.Component {
     }
 }
 
-//Breadcrumb default props
-Breadcrumb.defaultProps = {
-    style: null
-};
-
 //Breadcrumb item component
 export class BreadcrumbItem extends React.Component {
     render() {
         //Clone the props
         let props = getProps(this.props, ["className"]);
-        props.className = classNames("siimple-breadcrumb-item", this.props.className);;
+        props.className = classNames("siimple-breadcrumb-item", this.props.className);
         //Return the breadcrumb item
         return React.createElement("div", props, this.props.children);
     }
 }
-
-//Breadcrumb item default props
-BreadcrumbItem.defaultProps = {
-    style: null
-};
 
