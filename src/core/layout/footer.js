7d4d398de77f4e5c2edf760eee6c05ecf30a8339
jmjuanes <josemijuanes@gmail.com>
2018-10-27 21:16:56 +0200

    Fix paths to components utils

--- src/core/layout/footer.js
+++ src/core/layout/footer.js
@@ -1,8 +1,8 @@
 import React from "react";
 
 //Import components utils
-import classNames from "../class-names.js";
-import getProps from "../get-props.js";
+import classNames from "../../class-names.js";
+import getProps from "../../get-props.js";
 
 //Import footer styles
 import "siimple/scss/layout/_footer.scss";
