3966e91adf366bb4d1128cb8a370fc3e93b31073
jmjuanes <josemijuanes@gmail.com>
2018-10-27 19:53:52 +0200

    Move progress element to core folder. Minor changes

--- src/core/experiments/progress.js
+++ src/core/experiments/progress.js
@@ -1,13 +1,16 @@
 import React from "react";
-import {classNames, hyperscript as h} from "neutrine-utils";
-import {omit} from "kofi";
 
-import "siimple/scss/elements/_progress.scss";
+//Import component utils
+import classNames from "../../class-names.js";
+import getProps from "../../get-props.js";
+
+//Import rogress styles
+import "siimple/scss/experiments/_progress.scss";
 
 //Progress component
 export default class Progress extends React.Component {
     render() {
-        let props = omit(this.props, ["children", "className", "color", "completed", "striped", "velocity"]);
+        let props = getProps(this.props, ["className", "color", "completed", "striped", "velocity"]);
         let classList = ["siimple-progress"];
         //Check the color property
         if (typeof this.props.color === "string") {
@@ -22,17 +25,19 @@ export default class Progress extends React.Component {
                 classList.push("siimple-progress--striped");
             }
         }
-        let content = h("span", {style: {width: this.props.completed + "%"}}, this.props.children);
+        //Build the progress bar conent
+        let content = React.createElement("span", {"style": {"width": this.props.completed + "%"}}, this.props.children);
         props.className = classNames(classList, this.props.className);
-        return h("div", props, content);
+        //Return the progress element
+        return React.createElement("div", props, content);
     }
 }
 
 //Progress component default props 
 Progress.defaultProps = {
-    color: "blue",
-    completed: 0,
-    striped: false,
-    velocity: null
+    "color": "primary",
+    "completed": 0,
+    "striped": false,
+    "velocity": null
 };
 
