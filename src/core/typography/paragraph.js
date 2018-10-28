0f78d0c1a58b34e4b8c7921f2a5f43492bc16bff
jmjuanes <josemijuanes@gmail.com>
2018-10-28 10:51:11 +0100

    Move to core folder. Big rewrite

---
+++ src/core/typography/paragraph.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+//Import component utils
+import utils from "../utils.js";
+
+//Import paragraph styles
+import "siimple/scss/typography/_paragraph.scss";
+
+//Paragraph component
+export default function Paragraph (props) {
+    return utils.basicComponent("div", props, "siimple-paragraph");
+}
+
