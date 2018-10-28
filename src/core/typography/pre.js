f34d4b466a3127175898325adecf1cd794e0d559
jmjuanes <josemijuanes@gmail.com>
2018-10-28 10:52:28 +0100

    Move to core folder. Big rewrite

---
+++ src/core/typography/pre.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+//Import component utils
+import utils from "../utils.js";
+
+//Import pre component styles
+import "siimple/scss/typography/_pre.scss";
+
+//Export pre component 
+export default function Pre (props) {
+    return utils.basicComponent("pre", props, "siimple-pre");
+}
+
