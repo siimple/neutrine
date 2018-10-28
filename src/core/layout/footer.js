2804f951f90c9c096d15edf293a7384ae2fc565b
jmjuanes <josemijuanes@gmail.com>
2018-10-28 10:15:27 +0100

    Replace utils import. Add footer title, subtitle, link, group, paragraph and rule components

--- src/core/layout/footer.js
+++ src/core/layout/footer.js
@@ -1,17 +1,16 @@
 import React from "react";
 
 //Import components utils
-import classNames from "../../class-names.js";
-import getProps from "../../get-props.js";
+import utils from "../utils.js";
 
 //Import footer styles
 import "siimple/scss/layout/_footer.scss";
 
 //Footer layout component
-export default class Footer extends React.Component {
+export class Footer extends React.Component {
     render() {
         //Clone the footer props 
-        let props = getProps(this.props, ["className", "color", "size"]);
+        let props = utils.filterProps(this.props, ["className", "color", "size"]);
         //Initialize the footer class list
         let classList = ["siimple-footer"];
         //Check the color
@@ -23,7 +22,7 @@ export default class Footer extends React.Component {
             classList.push("siimple-footer--" + this.props.size.toLowerCase());
         }
         //Generate the footer classname
-        props.className = classNames(classList, this.props.className);
+        props.className = utils.classNames(classList, this.props.className);
         //Render the footer div
         return React.createElement("div", props, this.props.children);
     }
@@ -34,4 +33,34 @@ Footer.defaultProps = {
     "color": null, 
     "size": null 
 };
+
+//Footer title
+export function FooterTitle (props) {
+    return utils.basicComponent("div", props, "siimple-footer-title");
+}
+
+//Footer subtitle
+export function FooterSubtitle (props) {
+    return utils.basicComponent("div", props, "siimple-footer-subtitle");
+}
+
+//Footer group
+export function FooterGroup (props) {
+    return utils.basicComponent("div", props, "siimple-footer-group");
+}
+
+//Footer paragraph
+export function FooterParagraph (props) {
+    return utils.basicComponent("div", props, "siimple-footer-paragraph");
+}
+
+//Footer link
+export function FooterLink (props) {
+    return utils.basicComponent("a", props, "siimple-footer-link");
+}
+
+//Footer rule
+export function FooterRule (props) {
+    return utils.basicComponent("div", props, "siimple-footer-rule");
+}
 
