605017f596970cf3ace51212a4db1cd258ec1b72
jmjuanes <josemijuanes@gmail.com>
2018-10-28 10:49:46 +0100

    Move to core folder. Big rewrite

---
+++ src/core/typography/link.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+//Import component utils
+import utils from "../utils.js";
+
+//Import link styles
+import "siimple/scss/typography/_link.scss";
+
+//Export link component 
+export default function Link (props) {
+    return utils.basicComponent("a", props, "siimple-link");
+}
+
