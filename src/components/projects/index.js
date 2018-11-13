f789b3a5327661d2fdea9f39e5740b4b657c4aaa
jmjuanes <josemijuanes@gmail.com>
2018-11-14 00:17:39 +0100

    Add projects column component

--- src/components/projects/index.js
+++ src/components/projects/index.js
@@ -14,3 +14,24 @@ export function ProjectsRow (props) {
     return React.createElement("div", {"className": "neutrine-projects-row"}, props.children);
 }
 
+//Projects base column
+export function ProjectsColumn (props) {
+    //Initialize the column props
+    let columnProps = {
+        "className": "neutrine-projects-column",
+        "style": {
+            "flexGrow": (typeof props.size === "string") ? props.size : null
+        }
+    };
+    //Return the column component
+    return React.createElement("div", columnProps, props.children);
+}
+
+//Projects info column
+export function ProjectsInfoColumn (props) {
+    return React.createElement(ProjectsColumn, {"size": 2}, 
+        React.createElement("div", {"className": "neutrine-projects-info-title"}, props.title),
+        React.createElement("div", {"className": "neutrine-projects-info-description"}, props.description)
+    );
+}
+
