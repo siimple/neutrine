fc19b24068b900b4c39f2c7f37bb1ed05449a636
jmjuanes <josemijuanes@gmail.com>
2019-01-08 13:52:48 +0100

    Fixed icon: pass provided props to icon element

--- src/icon/index.js
+++ src/icon/index.js
@@ -1,11 +1,16 @@
 //Import dependencies
 import React from "react";
 
+//Import utils
+import * as reactUtils from "../utils/react.js";
+
 //Import icons
 import "siimple-icons/dist/siimple-icons.css";
 
 //Export icon component
 export default function Icon (props) {
+    //Filter props
+    let filteredProps = reactUtils.filterProps(props, ["className", "icon"]);
     //Initialize the icon class name list
     let iconClassNames = ["si"];
     //Add the icon class name
@@ -16,11 +21,10 @@ export default function Icon (props) {
     if (typeof props.className === "string") {
         iconClassNames.push(props.className);
     }
+    //Add the class names
+    filteredProps.className = reactUtils.classNames(iconClassNames, props.className);
     //Return the icon
-    return React.createElement("span", {
-        "className": iconClassNames.join(" "),
-        "style": props.style
-    });
+    return React.createElement("span", filteredProps);
 }
 
 //Icon default props
