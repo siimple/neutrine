49229f0dde81b30cabba5be9ada2ad07d5c23296
jmjuanes <josemijuanes@gmail.com>
2019-01-08 16:37:09 +0100

    Use basicComponent from react-utils to build the rule element

--- src/core/elements/rule.js
+++ src/core/elements/rule.js
@@ -1,8 +1,7 @@
 import React from "react";
 
 //Import components utils
-import classNames from "../../class-names.js";
-import getProps from "../../get-props.js";
+import * as reactUtils from "../../utils/react.js";
 
 //Import rule styles
 import "siimple/scss/elements/_rule.scss";
@@ -10,11 +9,8 @@ import "siimple/scss/elements/_rule.scss";
 //Close button class
 export default class Rule extends React.Component {
     render() {
-        //Clone the rule element props
-        let props = getProps(this.props, ["className"]);
-        props.className = classNames(["siimple-rule"], this.props.className);
         //Return the rule element
-        return React.createElement("div", props, null);
+        return reactUtils.basicComponent("div", props, "siimple-rule");
     }
 }
 
