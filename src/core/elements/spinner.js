bd6a30fa06fc888bd5666ab944982f24b1196db6
jmjuanes <josemijuanes@gmail.com>
2019-01-10 21:06:54 +0100

    Updated spinner component: use a function to render it

--- src/core/elements/spinner.js
+++ src/core/elements/spinner.js
@@ -1,29 +1,27 @@
 import React from "react";
 
 //Import components utils
-import classNames from "../../class-names.js";
-import getProps from "../../get-props.js";
+import * as reactUtils from "../../utils/react.js";
 
 //Import spinner styles
 import "siimple/scss/elements/_spinner.scss";
 
-//Spinner class
-export default class Spinner extends React.Component {
-    render() {
-        let props = getProps(this.props, ["className", "color", "size"]);
-        let classList = ["siimple-spinner"];
-        //Check the color attribute
-        if(typeof this.props.color === "string") {
-            classList.push("siimple-spinner--" + this.props.color);
-        }
-        //Check the size attribute
-        if (typeof this.props.size === "string") {
-            classList.push("siimple-spinner--" + this.props.size);
-        }
-        props.className = classNames(classList, this.props.className);
-        //Return the spinner element
-        return React.createElement("div", props, null);
+//Spinner element
+export default function Spinner (props) {
+    let props = reactUtils.getProps(this.props, ["className", "color", "size"]);
+    //Initialize the spinner class-list
+    let classList = ["siimple-spinner"];
+    //Check the color attribute
+    if(typeof this.props.color === "string") {
+        classList.push("siimple-spinner--" + this.props.color);
     }
+    //Check the size attribute
+    if (typeof this.props.size === "string") {
+        classList.push("siimple-spinner--" + this.props.size);
+    }
+    props.className = reactUtils.classNames(classList, this.props.className);
+    //Return the spinner element
+    return React.createElement("div", props, null);
 }
 
 //Spinner default props
