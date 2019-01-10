e4ee270d03de0864a5119650c302d017e51c84bd
jmjuanes <josemijuanes@gmail.com>
2019-01-10 21:14:50 +0100

    Changed to function component instead of class. Minor changes

--- src/core/elements/tag.js
+++ src/core/elements/tag.js
@@ -1,32 +1,29 @@
 import React from "react";
 
 //Import components utils
-import classNames from "../../class-names.js";
-import getProps from "../../get-props.js";
+import * as reactUtils from "../../utils/react.js";
 
 //Import tabg styles
 import "siimple/scss/elements/_tag.scss";
 
 //Export tag component 
-export default class Tag extends React.Component {
-    render() {
-        //Clone the tag component props
-        let props = getProps(this.props, ["className", "color", "rounded"]);
-        //Initialize the tag class list 
-        let classList = ["siimple-tag"];
-        //Check the color attribute
-        if (typeof this.props.color === "string") {
-            classList.push("siimple-tag--" + this.props.color.toLowerCase().trim());
-        }
-        //Check the rounded attribute
-        if (typeof this.props.rounded === "boolean" && this.props.rounded === true) {
-            classList.push("siimple-tag--rounded");
-        }
-        //Generate the tag classname
-        props.className = classNames(classList, this.props.className);
-        //Return the tag element
-        return React.createElement("div", props, this.props.children);
+export default function Tag (props) {
+    //Clone the tag component props
+    let newProps = reactUtils.getProps(props, ["className", "color", "rounded"]);
+    //Initialize the tag class list 
+    let classList = ["siimple-tag"];
+    //Check the color attribute
+    if (typeof props.color === "string") {
+        classList.push("siimple-tag--" + props.color.toLowerCase().trim());
     }
+    //Check the rounded attribute
+    if (typeof props.rounded === "boolean" && props.rounded === true) {
+        classList.push("siimple-tag--rounded");
+    }
+    //Generate the tag classname
+    newProps.className = reactUtils.classNames(classList, props.className);
+    //Return the tag element
+    return React.createElement("div", newProps, props.children);
 }
 
 //Tag default props
