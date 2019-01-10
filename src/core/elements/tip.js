d4014f4e4f374eb00b9ff380eb03866cb9527d93
jmjuanes <josemijuanes@gmail.com>
2019-01-10 21:16:53 +0100

    Render tip element using a component function. Minor changes

--- src/core/elements/tip.js
+++ src/core/elements/tip.js
@@ -1,30 +1,28 @@
 import React from "react";
 
 //Import components utils
-import classNames from "../../class-names.js";
-import getProps from "../../get-props.js";
+import * as reactUtils from "../../utils/react.js";
 
 //Import tip styles
 import "siimple/scss/elements/_tip.scss";
 
 //Tip class
-export default class Tip extends React.Component {
-    render() {
-        let props = getProps(this.props, ["className", "color", "icon"]);
-        let classList = ["siimple-tip"];
-        //Add the tip color
-        if (typeof this.props.color === "string") {
-            classList.push("siimple-tip--" + this.props.color.toLowerCase().trim());
-        }
-        //Add the tip icon
-        if (typeof this.props.icon === "string") {
-            classList.push("siimple-tip--" + this.props.icon.toLowerCase().trim());
-        }
-        //Generate the class names
-        props.className = classNames(classList, this.props.className);
-        //Return the tip element
-        return React.createElement("div", props, this.props.children);
+export default function Tip (props) {
+    let newProps = reactUtils.getProps(props, ["className", "color", "icon"]);
+    //Initialize tip class list
+    let classList = ["siimple-tip"];
+    //Add the tip color
+    if (typeof props.color === "string") {
+        classList.push("siimple-tip--" + props.color.toLowerCase().trim());
     }
+    //Add the tip icon
+    if (typeof props.icon === "string") {
+        classList.push("siimple-tip--" + props.icon.toLowerCase().trim());
+    }
+    //Generate the class names
+    newProps.className = reactUtils.classNames(classList, props.className);
+    //Return the tip element
+    return React.createElement("div", newProps, props.children);
 }
 
 //Initialize the tip element default props
