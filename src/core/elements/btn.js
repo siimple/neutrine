eea18c8c7df341e7716be6d409cad6424e6b8896
jmjuanes <josemijuanes@gmail.com>
2019-01-10 21:10:46 +0100

    Use a function to render the button element insteado of using a class

--- src/core/elements/btn.js
+++ src/core/elements/btn.js
@@ -7,34 +7,32 @@ import * as reactUtils from "../../utils/react.js";
 import "siimple/scss/elements/_btn.scss";
 
 //Button component
-export default class Btn extends React.Component {
-    render() {
-        //Initialize the button props 
-        let props = reactUrils.getProps(this.props, ["className", "color", "disabled", "fluid", "small"]);
-        //Initialize the class names list 
-        let classList = ["siimple-btn"];
-        //Add the button color
-        if (typeof this.props.color === "string") {
-            classList.push("siimple-btn--" + this.props.color.toLowerCase().trim());
-        }
-        //Add the button disabled option
-        if (this.props.disabled === true) {
-            classList.push("siimple-btn--disabled");
-        }
-        //Check the fluid property
-        if (this.props.fluid === true) {
-            //btn_props.style = { width: '100%', paddingLeft: '0px', paddingRight: '0px' };
-            classList.push("siimple-btn--fluid");
-        }
-        //Check the small property
-        if (this.props.small === true) {
-            classList.push("siimple-btn--small");
-        }
-        //Append the provided class names
-        props.className = reactUtils.classNames(classList, this.props.className);
-        //Return the button element
-        return React.createElement("div", props, this.props.children);
+export default function Btn (props) {
+    //Initialize the button props 
+    let newProps = reactUrils.getProps(props, ["className", "color", "disabled", "fluid", "small"]);
+    //Initialize the class names list 
+    let classList = ["siimple-btn"];
+    //Add the button color
+    if (typeof props.color === "string") {
+        classList.push("siimple-btn--" + props.color.toLowerCase().trim());
     }
+    //Add the button disabled option
+    if (props.disabled === true) {
+        classList.push("siimple-btn--disabled");
+    }
+    //Check the fluid property
+    if (props.fluid === true) {
+        //btn_props.style = { width: '100%', paddingLeft: '0px', paddingRight: '0px' };
+        classList.push("siimple-btn--fluid");
+    }
+    //Check the small property
+    if (props.small === true) {
+        classList.push("siimple-btn--small");
+    }
+    //Append the provided class names
+    newProps.className = reactUtils.classNames(classList, props.className);
+    //Return the button element
+    return React.createElement("div", newProps, props.children);
 }
 
 //Default properties values
