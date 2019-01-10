219d16cd0826cdc7937a842485b84306aad3e112
jmjuanes <josemijuanes@gmail.com>
2019-01-10 21:09:11 +0100

    Fixed bug parsing element props

--- src/core/elements/spinner.js
+++ src/core/elements/spinner.js
@@ -8,20 +8,20 @@ import "siimple/scss/elements/_spinner.scss";
 
 //Spinner element
 export default function Spinner (props) {
-    let props = reactUtils.getProps(this.props, ["className", "color", "size"]);
+    let newProps = reactUtils.getProps(props, ["className", "color", "size"]);
     //Initialize the spinner class-list
     let classList = ["siimple-spinner"];
     //Check the color attribute
-    if(typeof this.props.color === "string") {
-        classList.push("siimple-spinner--" + this.props.color);
+    if(typeof props.color === "string") {
+        classList.push("siimple-spinner--" + props.color);
     }
     //Check the size attribute
-    if (typeof this.props.size === "string") {
-        classList.push("siimple-spinner--" + this.props.size);
+    if (typeof props.size === "string") {
+        classList.push("siimple-spinner--" + props.size);
     }
-    props.className = reactUtils.classNames(classList, this.props.className);
+    newProps.className = reactUtils.classNames(classList, props.className);
     //Return the spinner element
-    return React.createElement("div", props, null);
+    return React.createElement("div", newProps, null);
 }
 
 //Spinner default props
