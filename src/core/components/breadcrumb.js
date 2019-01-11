910b0a232c5d1dd35a104c28c023f4fdf14642dd
jmjuanes <josemijuanes@gmail.com>
2019-01-11 11:42:51 +0100

    Switched to function component instead of class. Minor changes

--- src/core/components/breadcrumb.js
+++ src/core/components/breadcrumb.js
@@ -1,31 +1,18 @@
 import React from "react";
 
 //Import components utils
-import classNames from "../../class-names.js";
-import getProps from "../../get-props.js";
+import * as reactUtils from "../../utils/react.js";
 
 //Import breadcrumb styles
 import "siimple/scss/components/_breadcrumb.scss";
 
 //Breadcrumb parent component
-export class Breadcrumb extends React.Component {
-    render() {
-        //Initialize the breadcrumb props
-        let props = getProps(this.props, ["className"]);
-        props.className = classNames("siimple-breadcrumb", this.props.className);
-        //Return the breadcrumb parent
-        return React.createElement("div", props, this.props.children);
-    }
+export function Breadcrumb (props) {
+    return reactUtils.basicComponent("div", props, "siimple-breadcrumb");
 }
 
 //Breadcrumb item component
-export class BreadcrumbItem extends React.Component {
-    render() {
-        //Clone the props
-        let props = getProps(this.props, ["className"]);
-        props.className = classNames("siimple-breadcrumb-item", this.props.className);
-        //Return the breadcrumb item
-        return React.createElement("div", props, this.props.children);
-    }
+export function BreadcrumbItem (props) {
+    return reactUtils.basicComponent("div", props, "siimple-breadcrumb-item");
 }
 
