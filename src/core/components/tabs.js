c6e1680315ea7451237e73a1ca208023b8c8adcc
jmjuanes <josemijuanes@gmail.com>
2019-01-11 16:05:29 +0100

    Changed class components to function components in tabs and tabs-item component

--- src/core/components/tabs.js
+++ src/core/components/tabs.js
@@ -1,28 +1,25 @@
 import React from "react";
 
 //Import component utils
-import classNames from "../../class-names.js";
-import getProps from "../../get-props.js";
+import * as reactUtils from "../../utils/react.js";
 
 //Import tab styles
 import "siimple/scss/components/_tabs.scss";
 
 //Export tabs component
-export class Tabs extends React.Component {
-    render() {
-        //Clone the tabs props 
-        let props = getProps(this.props, ["className", "boxed"]);
-        //Initialize the tabs class list
-        let classList = ["siimple-tabs"];
-        //Check the boxed attribute
-        if (this.props.boxed === true) {
-            classList.push("siimple-tabs--boxed");
-        }
-        //Generate the classname
-        props.className = classNames(classList, this.props.className);
-        //Return the tabs element
-        return React.createElement("div", props, this.props.children);
+export function Tabs (props) {
+    //Clone the tabs props 
+    let newProps = reactUtils.filterProps(props, ["className", "boxed"]);
+    //Initialize the tabs class list
+    let classList = ["siimple-tabs"];
+    //Check the boxed attribute
+    if (props.boxed === true) {
+        classList.push("siimple-tabs--boxed");
     }
+    //Generate the classname
+    newProps.className = reactUtils.classNames(classList, props.className);
+    //Return the tabs element
+    return React.createElement("div", newProps, props.children);
 }
 
 //Tabs default props
@@ -31,21 +28,19 @@ Tabs.defaultProps = {
 };
 
 //Tabs item component 
-export class TabsItem extends React.Component {
-    render() {
-        //Extend the tabs item properties
-        let props = getProps(this.props, ["selected", "className"]);
-        //Initialize the tabs item class list
-        let classList = ["siimple-tabs-item"];
-        //Check the selected attribute 
-        if (this.props.selected === true) {
-            classList.push("siimple-tabs-item--selected");
-        }
-        //Generate the tabs item classname
-        props.className = classNames(classList, this.props.className);
-        //Return the tab item
-        return React.createElement("div", props, this.props.children);
+export function TabsItem (props) {
+    //Extend the tabs item properties
+    let newProps = reactUtils.filterProps(props, ["selected", "className"]);
+    //Initialize the tabs item class list
+    let classList = ["siimple-tabs-item"];
+    //Check the selected attribute 
+    if (props.selected === true) {
+        classList.push("siimple-tabs-item--selected");
     }
+    //Generate the tabs item classname
+    newProps.className = reactUtils.classNames(classList, props.className);
+    //Return the tab item
+    return React.createElement("div", newProps, props.children);
 }
 
 //Tabs item default props 
