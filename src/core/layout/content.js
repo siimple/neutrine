3e8f10b0103c6a45473c5448e985fb21266ab758
jmjuanes <josemijuanes@gmail.com>
2019-01-12 19:30:14 +0100

    Minor changes

--- src/core/layout/content.js
+++ src/core/layout/content.js
@@ -1,27 +1,25 @@
 import React from "react";
 
 //Import components utils
-import * as utils from "../utils.js";
+import * as reactUtils from "../../utils/react.js";
 
 //Import content styles
 import "siimple/scss/layout/_content.scss";
 
 //Content layout component
-export default class Content extends React.Component {
-    render() {
-        //Content props
-        let props = utils.filterProps(this.props, ["className", "size"]);
-        //Initialize the content class list
-        let classList = ["siimple-content"];
-        //Check the content size
-        if (typeof this.props.size === "string") {
-            classList.push("siimple-content--" + this.props.size.toLowerCase());
-        }
-        //Generate the content className
-        props.className = utils.classNames(classList, this.props.className);
-        //Render the content div
-        return React.createElement("div", props, this.props.children);
+export default function Content (props) {
+    //Content props
+    let newProps = reactUtils.filterProps(props, ["className", "size"]);
+    //Initialize the content class list
+    let classList = ["siimple-content"];
+    //Check the content size
+    if (typeof props.size === "string") {
+        classList.push("siimple-content--" + props.size.toLowerCase());
     }
+    //Generate the content className
+    newProps.className = reactUtils.classNames(classList, props.className);
+    //Render the content div
+    return React.createElement("div", newProps, props.children);
 }
 
 //Default props
