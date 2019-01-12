9127dea745750903ef3e20eba47bc710618981fc
jmjuanes <josemijuanes@gmail.com>
2019-01-12 19:35:04 +0100

    Updated reactUtils usage in navbar layout component. Minor changes

--- src/core/layout/navbar.js
+++ src/core/layout/navbar.js
@@ -1,31 +1,29 @@
 import React from "react";
 
 //Import components utils
-import * as utils from "../utils.js";
+import * as reactUtils from "../../utils/react.js";
 
 //Import navbar styles
 import "siimple/scss/layout/_navbar.scss";
 
 //Navbar default class
-export class Navbar extends React.Component {
-    render() {
-        //Clone the navbar props 
-        let props = utils.filterProps(this.props, ["className", "color", "size"]);
-        //Initialize the class list
-        let classList = ["siimple-navbar"];
-        //Check the color
-        if (typeof this.props.color === "string") {
-            classList.push("siimple-navbar--" + this.props.color.toLowerCase());
-        }
-        //Check the size
-        if (typeof this.props.size === "string") {
-            classList.push("siimple-navbar--" + this.props.size.toLowerCase());
-        }
-        //Generate the navbar classname
-        props.className = utils.classNames(classList, this.props.className);
-        //Render the navbar
-        return React.createElement("div", props, this.props.children);
+export function Navbar (props) {
+    //Clone the navbar props 
+    let newProps = reactUtils.filterProps(props, ["className", "color", "size"]);
+    //Initialize the class list
+    let classList = ["siimple-navbar"];
+    //Check the color
+    if (typeof props.color === "string") {
+        classList.push("siimple-navbar--" + props.color.toLowerCase());
     }
+    //Check the size
+    if (typeof props.size === "string") {
+        classList.push("siimple-navbar--" + props.size.toLowerCase());
+    }
+    //Generate the navbar classname
+    newProps.className = reactUtils.classNames(classList, props.className);
+    //Render the navbar
+    return React.createElement("div", newProps, props.children);
 }
 
 //Navbar default props
@@ -36,16 +34,16 @@ Navbar.defaultProps = {
 
 //Navbar title element
 export function NavbarTitle (props) {
-    return utils.basicComponent("div", props, "siimple-navbar-title");
+    return reactUtils.basicComponent("div", props, "siimple-navbar-title");
 }
 
 //Navbar subtitle class
 export function NavbarSubtitle (props) {
-    return utils.basicComponent("div", props, "siimple-navbar-subtitle");
+    return reactUtils.basicComponent("div", props, "siimple-navbar-subtitle");
 }
 
 //Navbar item element
 export function NavbarItem (props) {
-    return utils.basicComponent("div", props, "siimple-navbar-item");
+    return reactUtils.basicComponent("div", props, "siimple-navbar-item");
 }
 
