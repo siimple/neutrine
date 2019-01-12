f23eca09852c97a3773da0e7ab037880a99000c3
jmjuanes <josemijuanes@gmail.com>
2019-01-12 19:39:59 +0100

    Minor changes. Fixed import react-utils lib

--- src/core/typography/heading.js
+++ src/core/typography/heading.js
@@ -1,27 +1,25 @@
 import React from "react";
 
 //Import component utils
-import * as utils from "../utils.js";
+import * as utils from "../../utils/react.js";
 
 //Import heading styles
 import "siimple/scss/typography/_heading.scss";
 
 //Heading class
-export default class Heading extends React.Component {
-    render() {
-        //Initialize the header props
-        let props = utils.filterProps(this.props, ["type", "className"]);
-        //Initialize the header class list
-        let classList = [];
-        //Check the header type
-        if (typeof this.props.type === "string" && this.props.type.charAt(0).toLowerCase() === "h" && this.props.type.length === 2) {
-            classList.push("siimple-" + this.props.type.toLowerCase().trim());
-        }
-        //Generate the header classname
-        props.className = utils.classNames(classList, this.props.className);
-        //Return the heading element
-        return React.createElement("div", props, this.props.children);
+export default function Heading (props) {
+    //Initialize the header props
+    let newProps = utils.filterProps(props, ["type", "className"]);
+    //Initialize the header class list
+    let classList = [];
+    //Check the header type
+    if (typeof props.type === "string" && props.type.charAt(0).toLowerCase() === "h" && props.type.length === 2) {
+        classList.push("siimple-" + props.type.toLowerCase().trim());
     }
+    //Generate the header classname
+    newProps.className = utils.classNames(classList, props.className);
+    //Return the heading element
+    return React.createElement("div", newProps, props.children);
 }
 
 //Default heading props
