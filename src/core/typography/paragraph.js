699442f49c542dab7d20d78d8e5d162b1e13aab6
jmjuanes <josemijuanes@gmail.com>
2019-01-12 19:42:08 +0100

    Fixed import of react utils

--- src/core/typography/paragraph.js
+++ src/core/typography/paragraph.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 //Import component utils
-import * as utils from "../utils.js";
+import * as utils from "../../utils/react.js";
 
 //Import paragraph styles
 import "siimple/scss/typography/_paragraph.scss";
