1530f537f2001411447b6fd537e6e6974eb7e40a
jmjuanes <josemijuanes@gmail.com>
2019-01-12 19:17:47 +0100

    Minor changes

--- src/core/form/field.js
+++ src/core/form/field.js
@@ -1,42 +1,23 @@
 import React from "react";
 
 //Import components utils
-import classNames from "../../class-names.js";
-import getProps from "../../get-props.js";
+import * as reactUtils from "../../utils/react.js";
 
 //Import filed styles
 import "siimple/scss/form/_field.scss";
 
 //Form field component
-export class Field extends React.Component {
-    render() {
-        //Field props 
-        let props = getProps(this.props, ["className"]);
-        props.className = classNames("siimple-field", this.props.className);
-        //Return the field element
-        return React.createElement("div", props, this.props.children);
-    }
+export function Field (props) {
+    return reactUtils.basicComponent("div", props, "siimple-field");
 }
 
 //Field label component 
-export class FieldLabel extends React.Component {
-    render() {
-        //Field label props 
-        let props = getProps(this.props, ["className"]);
-        props.className = classNames("siimple-field-label", this.props.className);
-        //Return the field label element 
-        return React.createElement("div", props, this.props.children);
-    }
+export function FieldLabel (props) {
+    return reactUtils.basicComponent("div", props, "siimple-field-label");
 }
 
 //Field helper component
-export class FieldHelper extends React.Component {
-    render() {
-        //Field helper props
-        let props = getProps(this.props, ["className"]);
-        props.className = classNames("siimple-field-helper", this.props.className);
-        //Return the field helper element
-        return React.createElement("div", props, this.props.children);
-    }
+export function FieldHelper (props) {
+    return reactUtils.basicComponent("div", props, "siimple-field-helper");
 }
 
