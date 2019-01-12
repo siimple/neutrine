82dd72e2ffae7a238e00a85de86e15327570a5f7
jmjuanes <josemijuanes@gmail.com>
2019-01-12 19:46:49 +0100

    Minor changes

--- src/core/experiments/progress.js
+++ src/core/experiments/progress.js
@@ -1,36 +1,33 @@
 import React from "react";
 
 //Import component utils
-import classNames from "../../class-names.js";
-import getProps from "../../get-props.js";
+import * as reactUtils from "../../utils/react.js";
 
 //Import rogress styles
 import "siimple/scss/experiments/_progress.scss";
 
 //Progress component
-export default class Progress extends React.Component {
-    render() {
-        let props = getProps(this.props, ["className", "color", "completed", "striped", "velocity"]);
-        let classList = ["siimple-progress"];
-        //Check the color property
-        if (typeof this.props.color === "string") {
-            classList.push("siimple-progress--" + this.props.color.toLowerCase().trim());
-        }
-        //Check the striped property
-        if (typeof this.props.striped === "boolean" && this.props.striped === true) {
-            //Check the velocity value
-            if (typeof this.props.velocity === "string" && this.props.velocity !== "") {
-                classList.push("siimple-progress--striped-" + this.props.velocity.toLowerCase().trim());
-            } else {
-                classList.push("siimple-progress--striped");
-            }
-        }
-        //Build the progress bar conent
-        let content = React.createElement("span", {"style": {"width": this.props.completed + "%"}}, this.props.children);
-        props.className = classNames(classList, this.props.className);
-        //Return the progress element
-        return React.createElement("div", props, content);
+export default function Progress (props) {
+    let newProps = reactUtils.filterProps(props, ["className", "color", "completed", "striped", "velocity"]);
+    let classList = ["siimple-progress"];
+    //Check the color property
+    if (typeof props.color === "string") {
+        classList.push("siimple-progress--" + props.color.toLowerCase().trim());
     }
+    //Check the striped property
+    if (typeof props.striped === "boolean" && props.striped === true) {
+        //Check the velocity value
+        if (typeof props.velocity === "string" && props.velocity !== "") {
+            classList.push("siimple-progress--striped-" + props.velocity.toLowerCase().trim());
+        } else {
+            classList.push("siimple-progress--striped");
+        }
+    }
+    //Build the progress bar conent
+    let content = React.createElement("span", {"style": {"width": props.completed + "%"}}, props.children);
+    newProps.className = reactUtils.classNames(classList, props.className);
+    //Return the progress element
+    return React.createElement("div", newProps, content);
 }
 
 //Progress component default props 
