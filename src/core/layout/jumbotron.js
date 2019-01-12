a1a83333edafbdc6a35d459c094463c8e83cb995
jmjuanes <josemijuanes@gmail.com>
2019-01-12 19:33:54 +0100

    Changed react utils import and renamed as reactUtils. Minor changes

--- src/core/layout/jumbotron.js
+++ src/core/layout/jumbotron.js
@@ -1,31 +1,29 @@
 import React from "react";
 
 //Import components utils
-import * as utils from "../utils.js";
+import * as reactUtils from "../../utils/react.js";
 
 //Import jumbotron styles
 import "siimple/scss/layout/_jumbotron.scss";
 
 //Jumbotron base component
-export class Jumbotron extends React.Component {
-    render() {
-        //Clone the jumbotron props 
-        let props = utils.filterProps(this.props, ["className", "color", "size"]);
-        //Initialize the jumbotron class names list
-        let classList = ["siimple-jumbotron"];
-        //Check the jumbotron color
-        if (typeof this.props.color === "string") {
-            classList.push("siimple-jumbotron--" + this.props.color.toLowerCase());
-        }
-        //Check the jumbotron size property
-        if (typeof this.props.size === "string") {
-            classList.push("siimple-jumbotron--" + this.props.size.toLowerCase());
-        }
-        //Generate the jumbotron classname
-        props.className = utils.classNames(classList, this.props.className);
-        //Return the parent div
-        return React.createElement("div", props, this.props.children);
+export function Jumbotron (props) {
+    //Clone the jumbotron props 
+    let newProps = reactUtils.filterProps(props, ["className", "color", "size"]);
+    //Initialize the jumbotron class names list
+    let classList = ["siimple-jumbotron"];
+    //Check the jumbotron color
+    if (typeof props.color === "string") {
+        classList.push("siimple-jumbotron--" + props.color.toLowerCase());
     }
+    //Check the jumbotron size property
+    if (typeof props.size === "string") {
+        classList.push("siimple-jumbotron--" + props.size.toLowerCase());
+    }
+    //Generate the jumbotron classname
+    newProps.className = reactUtils.classNames(classList, props.className);
+    //Return the parent div
+    return React.createElement("div", newProps, props.children);
 }
 
 //Jumbotron default props
@@ -36,16 +34,16 @@ Jumbotron.defaultProps = {
 
 //Jumbotron title
 export function JumbotronTitle (props) {
-    return utils.basicComponent("div", props, "siimple-jumbotron-title");
+    return reactUtils.basicComponent("div", props, "siimple-jumbotron-title");
 }
 
 //Jumbotron subtitle 
 export function JumbotronSubtitle (props) {
-    return utils.basicComponent("div", props, "siimple-jumbotron-subtitle");
+    return reactUtils.basicComponent("div", props, "siimple-jumbotron-subtitle");
 }
 
 //Jumbotron detail component 
 export function JumbotronDetail (props) {
-    return utils.basicComponent("div", props, "siimple-jumbotron-detail");
+    return reactUtils.basicComponent("div", props, "siimple-jumbotron-detail");
 }
 
