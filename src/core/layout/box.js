1ed521ff90596de7e05598fd608f8810edebc1e6
jmjuanes <josemijuanes@gmail.com>
2019-01-12 19:28:50 +0100

    Minor changes on box layout component

--- src/core/layout/box.js
+++ src/core/layout/box.js
@@ -1,26 +1,24 @@
 import React from "react";
 
 //Import components utils
-import * as utils from "../utils.js";
+import * as reactUtils from "../../utils/react.js";
 
 //Import styles
 import "siimple/scss/layout/_box.scss";
 
 //Box layout class
-export class Box extends React.Component {
-    render() {
-        //Clone the box props 
-        let props = utils.filterProps(this.props, ["className", "color"]);
-        let classList = ["siimple-box"];
-        //Check the box color property
-        if (typeof this.props.color === "string") {
-            classList.push("siimple-box--" + this.props.color.toLowerCase());
-        }
-        //Save the box className
-        props.className = utils.classNames(classList, this.props.className);
-        //Return the box component
-        return React.createElement("div", props, this.props.children);
+export function Box (props) {
+    //Clone the box props 
+    let newProps = reactUtils.filterProps(props, ["className", "color"]);
+    let classList = ["siimple-box"];
+    //Check the box color property
+    if (typeof props.color === "string") {
+        classList.push("siimple-box--" + props.color.toLowerCase());
     }
+    //Save the box className
+    newProps.className = reactUtils.classNames(classList, props.className);
+    //Return the box component
+    return React.createElement("div", newProps, props.children);
 }
 
 //Default props
@@ -30,16 +28,16 @@ Box.defaultProps = {
 
 //Box title
 export function BoxTitle (props) {
-    return utils.basicComponent("div", props, "siimple-box-title");
+    return reactUtils.basicComponent("div", props, "siimple-box-title");
 }
 
 //Box subtitle 
 export function BoxSubtitle (props) {
-    return utils.basicComponent("div", props, "siimple-box-subtitle");
+    return reactUtils.basicComponent("div", props, "siimple-box-subtitle");
 }
 
 //Box detail component 
 export function BoxDetail (props) {
-    return utils.basicComponent("div", props, "siimple-box-detail");
+    return reactUtils.basicComponent("div", props, "siimple-box-detail");
 }
 
