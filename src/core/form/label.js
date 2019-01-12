75705d7d7e2caeb4a3af2b9726bdd8b2142ac0a6
jmjuanes <josemijuanes@gmail.com>
2019-01-12 19:25:00 +0100

    Minor changes

--- src/core/form/label.js
+++ src/core/form/label.js
@@ -1,20 +1,13 @@
 import React from "react";
 
 //Import components utils
-import classNames from "../../class-names.js";
-import getProps from "../../get-props.js";
+import * as reactUtils from "../../utils/react.js";
 
 //Import label styles
 import "siimple/scss/form/_label.scss";
 
 //Label component
-export default class Label extends React.Component {
-    render() {
-        //Initialize the label props
-        let props = getProps(this.props, ["className"]);
-        props.className = classNames("siimple-label", this.props.className);
-        //Return the label element
-        return React.createElement("label", props, this.props.children);
-    }
+export default function Label (props) {
+    return reactUtils.basicComponent("div", props, "siimple-label");
 }
 
