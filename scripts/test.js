c387940adb565f8aacf0000ec465df87868e1462
jmjuanes <josemijuanes@gmail.com>
2019-01-25 12:35:46 +0100

    Minor changes

--- scripts/test.js
+++ scripts/test.js
@@ -13,6 +13,8 @@ process.nextTick(function () {
     });
     //Register static folders routes
     app.use("/dist", express.static(path.join(process.cwd(), "dist")));
+    app.use("/node_modules", express.static(path.join(process.cwd(), "node_modules")));
+    app.use("/bower_components", express.static(path.join(process.cwd(), "bower_components")));
     app.use("/", express.static(path.join(process.cwd(), "test")));
     //Not found middleware
     app.use("*", function (req, res) {
@@ -20,7 +22,7 @@ process.nextTick(function () {
     });
     //Listen to test port
     app.listen(3000, function () {
-        return console.log("Neutrine Testing service listening on port " + config.port);
+        return console.log("Neutrine Testing service listening on port 3000");
     });
 });
 
