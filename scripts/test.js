0247261884c2b997e17f9d466df7ed517cb0521c
jmjuanes <josemijuanes@gmail.com>
2019-01-25 12:13:47 +0100

    Init test server script

---
+++ scripts/test.js
@@ -0,0 +1,27 @@
+//Import dependencies
+let express = require("express");
+let path = require("path");
+
+//Server render
+process.nextTick(function () {
+    let app = express();
+    //Enable cors
+    app.use(function (req, res, next) {
+        res.header("Access-Control-Allow-Origin", "*");
+        res.header("Access-Control-Allow-Headers", "X-Requested-With,content-type");
+        return next();
+    });
+    //Register static folders routes
+    app.use("/dist", express.static(path.join(process.cwd(), "dist")));
+    app.use("/", express.static(path.join(process.cwd(), "test")));
+    //Not found middleware
+    app.use("*", function (req, res) {
+        return res.sendStatus(404);
+    });
+    //Listen to test port
+    app.listen(3000, function () {
+        return console.log("Neutrine Testing service listening on port " + config.port);
+    });
+});
+
+
