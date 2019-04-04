df2fa074759fc4a0ebe23da6a8a4499f18cbeb10
jmjuanes <josemijuanes@gmail.com>
2019-04-04 11:16:10 +0200

    Added ready method to helpers

--- src/helpers.js
+++ src/helpers.js
@@ -77,3 +77,19 @@ export function createMergedElement (tag, originalProps, newProps) {
     return React.createElement(tag, mergeProps(originalProps, newProps), originalProps.children);
 }
 
+//Execute the provided function when the DOM is ready
+export function ready (callback) {
+    //Check if the state is not loading
+    if (document.readyState !== "loading") {
+        return callback.call(null, null);
+    }
+    //Register the event listener
+    document.addEventListener("DOMContentLoaded", function (event) {
+        if (document.readyState === "loading") {
+            return;
+        }
+        //Call the provided callback function
+        return callback.call(null, event);
+    });
+}
+
