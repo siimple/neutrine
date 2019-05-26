b2ca32b2403a224193c7190455907f64aeb5f9d9
jmjuanes <josemijuanes@gmail.com>
2019-05-26 13:43:09 +0200

    Big rewrite

--- src/layout/toolbar/index.js
+++ src/layout/toolbar/index.js
@@ -1,5 +1,7 @@
 //Import dependencies
 import React from "react";
+//import {Icon} from "../../icon/index.js";
+import * as helpers from "../../helpers.js";
 
 //Import toolbar styles
 import "./style.scss";
@@ -7,116 +9,78 @@ import "./style.scss";
 //Base class
 let baseClass = "neutrine-layout-toolbar";
 
-//Export layout toolbar component
-export class Toolbar extends React.Component {
-    constructor(props) {
-        super(props);
-        //Initialize the toolbar status
-        this.state = {
-            "collapsed": true
-        };
+//Export toolbar wrapper component
+export function ToolbarContainer (props) {
+    //Toolbar class styles
+    let classList = [baseClass + "-container"];
+    //Check if toolbar is collapsed
+    if (props.collapsed === true) {
+        classList.push(baseClass + "-container--collapsed");
     }
-    //Render the toolbar container component
-    renderContainer() {
-        let self = this;
-        //Initialize the container props
-        let containerProps = {
-            "className": baseClass + "-container"
-        };
-        //Return the sidebar element
-        return React.createElement("div", containerProps, this.renderItems(), this.renderToggle());
-    }
-    //Render the toolbar items
-    renderItems() {
-        let self = this;
-        //Build all groups links
-        let groups = this.props.groups.map(function (group, groupIndex) { 
-            //Build the group title element
-            let title = React.createElement("div", {"className": baseClass + "-group"}, group);
-            //Build the group items list
-            let items = [];
-            self.props.items.forEach(function (item, index) {
-                //Check if this item is not on this group
-                if (typeof item.group !== "string" || item.group !== group) {
-                    return null;
-                }
-                //Initialize the item props
-                let props = {
-                    "className": [baseClass + "-link"],
-                    "onClick": function () {
-                        if (typeof self.props.onClick === "function") {
-                            return self.props.onCLick(item, index);
-                        }
-                    },
-                    "key": index
-                };
-                //Check if this item is active
-                if (key === self.props.current) {
-                    props.className.push(baseClass + "-link--active");
-                }
-                //Check for custom item classname
-                if (typeof item.className === "string") {
-                    props.className.push(item.className);
-                }
-                //Check for custom item icon
-                if (typeof item.icon === "string") {
-                    props.style = {
-                        "backgroundImage": "url('" + item.icon + "')"
-                    };
-                }
-                //Join the classnames
-                props.className = props.className.join(" ");
-                //Save the button component
-                items.push(React.createElement("div", props, item["title"])); 
-            });
-            //Return the group parent
-            return React.createElement(React.Fragment, {"key": groupIndex}, title, items);
-        });
-        //Return all items
-        return React.createElement(React.Fragment, {}, groups);
-    }
-    //Render the toolbar toggle button
-    renderToggle() {
-        let self = this;
-        //Initialize the toggle button props
-        let toggleProps = {
-            "className": baseClass + "-toggle",
-            "onClick": function (event) {
-                //console.log("toggle clicked");
-                //Get the current collapsed value
-                let currentCollapsed = self.state.collapsed;
-                //Update the state
-                return self.setState({
-                    "collapsed": !currentCollapsed
-                });
-            }
-        };
-        //Return the toggle toolbar
-        return React.createElement("div", toggleProps);
-    }
-    //Render the toolbar component
-    render() {
-        //Initialize the toolbar props
-        let toolbarProps = {
-            "className": [baseClass]
-        };
-        //Check if the layout is collapsed
-        if (this.state.collapsed === true) {
-            toolbarProps.className.push(baseClass + "--collapsed");
-        }
-        //Merge the classnames
-        toolbarProps.className = toolbarProps.className.join(" ");
-        //Return the toolbar wrapper
-        return React.createElement("div", toolbarProps, this.renderContainer(), this.props.children);
-    }
+    //Return the toolbar element
+    return React.createElement("div", {"className": classList.join(" ")}, props.children);
 }
 
-//Layout default props
-Toolbar.defaultProps = {
-    "color": "blue",
-    "items": [],
-    "groups": [],
-    "current": null,
+//Toolbar container default props
+ToolbarContainer.defaultProps = {
+    "collapsed": true
+};
+
+//Export toolbar 
+export function Toolbar (props) {
+    //Initialize the toolbar props
+    let toolbarProps = {
+        "className": baseClass
+    };
+    //Return the sidebar element
+    return React.createElement("div", toolbarProps, props.children);
+}
+
+//Toolbar default props
+Toolbar.defaultProps = {};
+
+//Toolbar toggle
+export function ToolbarToggle (props) {
+    return React.createElement("div", {
+        "className": baseClass + "-toggle",
+        "onClick": props.onClick
+    });
+}
+
+//Toolbar toggle default props
+ToolbarToggle.defaultProps = {
     "onClick": null
 };
+
+//Toolbar item
+export function ToolbarItem (props) {
+    //Initialize the item classlist
+    let classList = [baseClass + "-item"];
+    //Check if this item is active
+    if (props.active === true) {
+        classList.push(baseClass + "-item--active");
+    }
+    //Build the item props
+    let itemProps = {
+        "className": helpers.classNames(classList, props.className),
+        "style": {
+            "backgroundImage": "url('" + props.icon + "')"
+        },
+        "onClick": props.onClick
+    };
+    //Return the item
+    return React.createElement("div", itemProps, props.children); 
+}
+
+//Toolbar item defualt props
+ToolbarItem.defaultProps = {
+    "onClick": null,
+    "active": false,
+    "icon": null
+};
+
+//Toolbar group
+export function ToolbarGroup (props) {
+    return React.createElement("div", {"className": baseClass + "-group"}, props.children);
+}
 
