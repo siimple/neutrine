516a7427677d61e2f5e933c34a882574385acbf5
jmjuanes <josemijuanes@gmail.com>
2019-05-26 14:24:34 +0200

    Updated toolbar test

--- test/packages/layout-toolbar/index.js
+++ test/packages/layout-toolbar/index.js
@@ -1,9 +1,15 @@
 Neutrine.ready(function () {
     let ToolbarTest = function (props) { 
         return (
-            <Neutrine.Toolbar>
+            <Neutrine.ToolbarContainer collapsed={false}>
+                <Neutrine.Toolbar>
+                    <Neutrine.ToolbarGroup>Group 1</Neutrine.ToolbarGroup>
+                    <Neutrine.ToolbarItem active={true}>Item 1</Neutrine.ToolbarItem>
+                    <Neutrine.ToolbarItem active={false}>Item 2</Neutrine.ToolbarItem>
+                    <Neutrine.ToolbarToggle />
+                </Neutrine.Toolbar>
                 Hello world
-            </Neutrine.Toolbar>
+            </Neutrine.ToolbarContainer>
         );
     };
     //Mount toolbar test component
