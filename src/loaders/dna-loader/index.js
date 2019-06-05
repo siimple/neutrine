67955dd22745a35a3a471d51f0e3d7b878fa6a26
Josemi <josemijuanes@gmail.com>
2019-06-05 11:44:22 +0200

    Changed export of dna-loader component

--- src/loaders/dna-loader/index.js
+++ src/loaders/dna-loader/index.js
@@ -5,31 +5,29 @@ import React from "react";
 import "./style.scss";
 
 //DNA loader component
-export class DNALoader extends React.Component {
-    render() {
-        //Generate all the nucleotides
-        let children = Array.apply(null, Array(10)).map(function (value, index) {
-            //Initialize the nucleotide props
-            let nucleotideProps = {
-                "className": "neutrine-dna-loader-nucleotide",
-                "key": index
-            };
-            //Return the nucleotide
-            return React.createElement("div", nucleotideProps, React.createElement("div", {}, null));
-        });
-        //Class list
-        let classList = ["neutrine-dna-loader"];
-        //Check the DNA-loader color property 
-        if (typeof this.props.color === "string") {
-            classList.push("neutrine-dna-loader--" + this.props.color);
-        }
-        //Check for DNA stick property
-        if (typeof this.props.stick === "boolean" && this.props.stick === true) {
-            classList.push("neutrine-dna-loader--stick");
-        }
-        //Return the loader wrapper
-        return React.createElement("div", {"className": classList.join(" ")}, children);
+export function DNALoader (props) {
+    //Generate all the nucleotides
+    let children = Array.apply(null, Array(10)).map(function (value, index) {
+        //Initialize the nucleotide props
+        let nucleotideProps = {
+            "className": "neutrine-dna-loader-nucleotide",
+            "key": index
+        };
+        //Return the nucleotide
+        return React.createElement("div", nucleotideProps, React.createElement("div", {}, null));
+    });
+    //Class list
+    let classList = ["neutrine-dna-loader"];
+    //Check the DNA-loader color property 
+    if (typeof props.color === "string") {
+        classList.push("neutrine-dna-loader--" + props.color);
     }
+    //Check for DNA stick property
+    if (typeof props.stick === "boolean" && props.stick === true) {
+        classList.push("neutrine-dna-loader--stick");
+    }
+    //Return the loader wrapper
+    return React.createElement("div", {"className": classList.join(" ")}, children);
 }
 
 //DNA Loader default props
