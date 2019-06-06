656d535060d54a2d333ad63f008ccf012f999685
Josemi <josemijuanes@gmail.com>
2019-06-06 11:30:32 +0200

    Switched to non-jsx component

--- src/components/bottom-side/index.js
+++ src/components/bottom-side/index.js
@@ -21,6 +21,35 @@ export class BottomSide extends React.Component {
             "collapsed": !this.state.collapsed
         });
     }
+    //Render the title
+    renderiHeaderTitle() {
+        return this.title;
+    }
+    //Render the header chevron
+    renderHeaderChevron() {
+        return React.createElement("div", {
+            "className": baseClass + "-chevron"
+        });
+    }
+    //Render the bottom side header
+    renderHeader() {
+        //Build the header props
+        let props = {
+            "className": baseClass + "-header",
+            "onClick": this.handleCollapse
+        };
+        //Return the header
+        return React.createElement("div", props, this.renderHeaderTitle(), this.renderHeaderChevron());
+    }
+    //Render the body component
+    renderBody() {
+        //Build the body props
+        let props = {
+            "className": baseClass + "-body"
+        };
+        //Return the body component
+        return React.createElement("div", props, this.props.children);
+    }
     //Render the bottom side component
     render() {
         let classList = [baseClass];
@@ -29,17 +58,7 @@ export class BottomSide extends React.Component {
             classList.push(baseClass + "--collapsed");
         }
         //Return the bottom side component
-        return (
-            <div className={classList.join(" ")}>
-                <div className={baseClass + "-header"} onClick={this.handleCollapse}>
-                    {this.props.title}
-                    <div className={baseClass + "-chevron"} />
-                </div>
-                <div className={baseClass + "-body"}>
-                    {this.props.children}
-                </div>
-            </div>
-        );
+        return React.createElement("div", {"className": classList.join(" ")}, this.renderHeader(), this.renderBody());
     }
 }
 
