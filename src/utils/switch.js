30c4ec31dccfb00b8d324a7f18238a9c096c32fd
Josemi <josemijuanes@gmail.com>
2019-07-12 11:31:50 +0200

    Init switch utility component

---
+++ src/utils/switch.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+//Export Switch component
+export function Switch (props) {
+    let match = null;
+    //Check all children elements
+    React.Children.forEach(props.children, function (child) {
+        if (match === null) {
+            //Check the case condition
+            if (children.props.condition === true) {
+                match = child;
+            }
+        }
+    });
+    //Return the matched case
+    return match;
+}
+
+//Switch case component
+export function SwitchCase (props) {
+    if (typeof props.render === "function") {
+        return props.render();
+    }
+}
+
+//Switch case default props
+SwitchCase.defaultProps = {
+    "condition": false,
+    "render": null
+};
+
